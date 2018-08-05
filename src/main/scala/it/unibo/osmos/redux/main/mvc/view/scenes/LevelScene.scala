bb1d4ab8f3294cbe32be4d781fdf7a1b6db7d2b6
Marco Galassi <marco.galassi6@studio.unibo.it>
2018-08-05 22:42:43 +0200

    Added LevelStage stub class

---
+++ src/main/scala/it/unibo/osmos/redux/main/mvc/view/scenes/LevelScene.scala
@@ -0,0 +1,11 @@
+package it.unibo.osmos.redux.main.mvc.view.scenes
+
+import scalafx.application.Platform
+import scalafx.stage.Stage
+
+/**
+  * This scene holds and manages a single level
+  */
+class LevelScene(override val parentStage: Stage) extends BaseScene(parentStage) {
+
+}
