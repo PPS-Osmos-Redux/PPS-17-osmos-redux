ada47c69f443605fca3f834599a302d0f38e05cc
Marco Galassi <marco.galassi6@studio.unibo.it>
2018-08-06 01:03:48 +0200

    Updated LevelScene with basic level context management

--- src/main/scala/it/unibo/osmos/redux/main/mvc/view/scenes/LevelScene.scala
+++ src/main/scala/it/unibo/osmos/redux/main/mvc/view/scenes/LevelScene.scala
@@ -1,5 +1,6 @@
 package it.unibo.osmos.redux.main.mvc.view.scenes
 
+import it.unibo.osmos.redux.main.mvc.view.drawables.CircleDrawable
 import it.unibo.osmos.redux.main.mvc.view.levels.{LevelContext, LevelContextListener}
 import scalafx.application.Platform
 import scalafx.scene.canvas.Canvas
@@ -13,13 +14,15 @@ class LevelScene(override val parentStage: Stage) extends BaseScene(parentStage)
 
   val canvas: Canvas = new Canvas
   val levelContext: LevelContext = LevelContext(this)
+  val circleDrawable: CircleDrawable = new CircleDrawable(canvas.graphicsContext2D)
 
   onMouseClicked = mouseEvent => levelContext.pushMouseEvent(mouseEvent)
 
   override def onDrawEntities(drawables: Seq[(Double, Double, Double, Double)]): Unit = {
     /* We must draw to the screen the entire collection */
     Platform.runLater({
-
+      canvas.graphicsContext2D.clearRect(parentStage.getX, parentStage.getY, parentStage.getWidth, parentStage.getHeight)
+      drawables foreach(e => circleDrawable.draw(e._1, e._2, e._3, e._4))
     })
   }
 }
