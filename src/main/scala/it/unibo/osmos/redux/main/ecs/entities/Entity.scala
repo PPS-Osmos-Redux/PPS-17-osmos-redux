52932a26ee3119dc3f0e36119b8c35ab3da34cc0
Marco Galassi <marco.galassi6@studio.unibo.it>
2018-08-05 10:23:37 +0200

    Created packages for ECS; Added basic ECS traits

---
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/entities/Entity.scala
@@ -0,0 +1,8 @@
+package it.unibo.osmos.redux.main.ecs.entities
+
+/**
+  * Entity base trait
+  */
+trait Entity {
+
+}
---
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/systems/System.scala
@@ -0,0 +1,8 @@
+package it.unibo.osmos.redux.main.ecs.systems
+
+/**
+  * System base trait
+  */
+class System {
+
+}
