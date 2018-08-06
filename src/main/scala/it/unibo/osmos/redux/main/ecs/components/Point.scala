49f3ccef936dd50b5283e77d5340bc25bdb4ca4b
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-06 18:43:47 +0200

    add comments

--- src/main/scala/it/unibo/osmos/redux/main/ecs/components/Point.scala
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/components/Point.scala
@@ -1,7 +1,20 @@
 package it.unibo.osmos.redux.main.ecs.components
 
+/**
+  * Cartesian point
+  */
 trait Point {
+
+  /**
+    * Getter. Return the x coordinate of the point
+    * @return x coordinate
+    */
   def x: Int
+
+  /**
+    * Getter. Return the y coordinate of the point
+    * @return y coordinate
+    */
   def y: Int
 }
 
