6a6de766d5fa598215e4d18115d18a8ba76c68fd
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-06 18:48:30 +0200

    add comments

--- src/main/scala/it/unibo/osmos/redux/main/ecs/components/Dimension.scala
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/components/Dimension.scala
@@ -1,7 +1,20 @@
 package it.unibo.osmos.redux.main.ecs.components
 
+/**
+  * Component Dimension (radius of the sphere)
+  */
 trait Dimension {
+
+  /**
+    * Getter. Return the radius of the sphere
+    * @return the radius
+    */
   def radius: Int
+
+  /**
+    * Setter. Set the new value of the radius
+    * @param radius the new radius
+    */
   def radius_(radius: Int): Unit
 }
 
