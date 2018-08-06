ec0878c7b6fe3772cdaee717a26404bd8cb79e81
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-06 18:46:09 +0200

    add comments

--- src/main/scala/it/unibo/osmos/redux/main/ecs/components/Position.scala
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/components/Position.scala
@@ -1,7 +1,20 @@
 package it.unibo.osmos.redux.main.ecs.components
 
+/**
+  * Component Position (coordinates of the sphere's center)
+  */
 trait Position {
+
+  /**
+    * Getter. Return the center of the sphere
+    * @return the center
+    */
   def point: Point
+
+  /**
+    * Setter. Set the new center of the speed
+    * @param point the new center
+    */
   def point_(point: Point): Unit
 }
 
