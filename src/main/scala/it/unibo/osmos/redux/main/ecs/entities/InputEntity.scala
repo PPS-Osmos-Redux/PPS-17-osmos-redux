5092cf4c83f842807bfd9cce87859134420dd666
Giacomo <giacomo.venturini2@studio.unibo.it>
2018-08-07 21:04:49 +0200

    add traits to group ECS Components traits

---
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/entities/InputEntity.scala
@@ -0,0 +1,5 @@
+package it.unibo.osmos.redux.main.ecs.entities
+
+trait InputEntity extends PositionEntity with SpeedEntity with AccelerationEntity {
+
+}
---
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/entities/MovementEntity.scala
@@ -0,0 +1,5 @@
+package it.unibo.osmos.redux.main.ecs.entities
+
+trait MovementEntity extends AccelerationEntity with PositionEntity with SpeedEntity{
+
+}
