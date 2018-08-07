4285b1de569cc8fcb807c35855c46e8bba0f5855
Giacomo <giacomo.venturini2@studio.unibo.it>
2018-08-07 20:47:07 +0200

    implement base Entity

--- src/main/scala/it/unibo/osmos/redux/main/ecs/entities/Entity.scala
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/entities/Entity.scala
@@ -1,8 +1,21 @@
 package it.unibo.osmos.redux.main.ecs.entities
 
+import java.util.UUID
+
 /**
   * Entity base trait
   */
 trait Entity {
 
+  def getUUID: UUID
+}
+
+object Entity {
+  def apply(): Entity = EntityImpl()
+
+  private case class EntityImpl() extends Entity {
+    private val EntityUUID: UUID = UUID.randomUUID()
+
+    override def getUUID: UUID = EntityUUID
+  }
 }
