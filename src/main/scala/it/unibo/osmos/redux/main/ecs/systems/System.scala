afc9826cfe8e7989a139a89e28c0639d3dc9899f
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-08 16:33:25 +0200

    implement abstract system with abstract method update

--- src/main/scala/it/unibo/osmos/redux/main/ecs/systems/System.scala
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/systems/System.scala
@@ -1,8 +1,23 @@
 package it.unibo.osmos.redux.main.ecs.systems
 
-/**
-  * System base trait
-  */
-trait System {
+import it.unibo.osmos.redux.main.ecs.entities.EMEvents.{EntityCreated, EntityDeleted}
+import it.unibo.osmos.redux.main.ecs.entities._
 
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Abstract system of generic entity
+  */
+abstract class System[T>:Entity] (val entityManager: Observable, val priority: Int) extends Observer{
+
+  protected var entities: ListBuffer[T] = ListBuffer()
+
+  entityManager.subscribe(this, Class[T])
+
+  override def notify(event: EMEvents.EntityManagerEvent): Unit = event match {
+    case event: EntityCreated => entities += event.entity
+    case event: EntityDeleted => entities -= event.entity
+  }
+
+  def update()
 }
