4254e6bfae9b204f2064d1ce2796c7b9fbbd5d01
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-09 17:58:09 +0200

    add entity type component

---
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/components/TypeComponent.scala
@@ -0,0 +1,7 @@
+package it.unibo.osmos.redux.main.ecs.components
+
+/**
+  * Component for entity's type
+  * @param typeEntity entity's type
+  */
+case class TypeComponent(typeEntity: EntityType.Value)
