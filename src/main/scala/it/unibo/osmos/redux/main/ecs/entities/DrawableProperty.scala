d454db71fff3c1b92a5b47d4abe3a2caeee68f7a
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-09 18:05:31 +0200

    now DrawProperty extends Type property

--- src/main/scala/it/unibo/osmos/redux/main/ecs/entities/DrawableProperty.scala
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/entities/DrawableProperty.scala
@@ -3,6 +3,6 @@ package it.unibo.osmos.redux.main.ecs.entities
 /**
   * Trait representing the properties needed by an entity to be drawable
   */
-trait DrawableProperty extends Position with Dimension with Visible {
+trait DrawableProperty extends Position with Dimension with Visible with Type{
 
 }
