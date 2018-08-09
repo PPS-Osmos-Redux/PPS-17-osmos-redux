136099774e3d7d8f8d61451d5e21121a80ddd9b3
Andrea Procucci <Andrea Procucci>
2018-08-09 17:22:45 +0200

    Added resource folder and method for read file + added lib for json conversion

--- src/main/scala/it/unibo/osmos/redux/main/mvc/controller/Controller.scala
+++ src/main/scala/it/unibo/osmos/redux/main/mvc/controller/Controller.scala
@@ -1,5 +1,13 @@
 package it.unibo.osmos.redux.main.mvc.controller
+import it.unibo.osmos.redux.main.ecs.components._
+import it.unibo.osmos.redux.main.ecs.entities.{CellEntity, PlayerCellEntity}
+import it.unibo.osmos.redux.main.mvc.view.View
+import it.unibo.osmos.redux.main.mvc.view.levels.LevelContext
+import it.unibo.osmos.redux.main.utils.Point
+import spray.json._
+import DefaultJsonProtocol._
 
+import scala.io.Source
 import it.unibo.osmos.redux.main.ecs.components._
 import it.unibo.osmos.redux.main.ecs.entities.{CellEntity, PlayerCellEntity}
 import it.unibo.osmos.redux.main.mvc.view.levels.LevelContext
@@ -39,5 +47,17 @@ case class ControllerImpl() extends Controller {
     val ce = CellEntity(a, c, d, p, s, v)
     val pce = PlayerCellEntity(a,c,dPlayer,pPlayer,s,v)
     List(ce,pce)
+  }
+}
+
+object fileReader {
+  /**
+    * Reads a file from the resources folder
+    * @param filename the name of the file
+    * @return Content of the file
+    */
+  def loadResource(filename: String): String = {
+    val source = Source.fromURL(getClass.getResource(filename))
+    try source.mkString finally source.close()
   }
 }
