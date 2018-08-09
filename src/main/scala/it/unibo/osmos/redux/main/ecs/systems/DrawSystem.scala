5a0d89aab6a88f38756b31a64519f97fc76eb321
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-09 21:21:04 +0200

    implement method update

--- src/main/scala/it/unibo/osmos/redux/main/ecs/systems/DrawSystem.scala
+++ src/main/scala/it/unibo/osmos/redux/main/ecs/systems/DrawSystem.scala
@@ -3,6 +3,7 @@ package it.unibo.osmos.redux.main.ecs.systems
 import it.unibo.osmos.redux.main.ecs.entities.EMEvents.{EntityCreated, EntityDeleted}
 import it.unibo.osmos.redux.main.ecs.entities._
 import it.unibo.osmos.redux.main.mvc.view.levels.LevelContext
+import it.unibo.osmos.redux.main.mvc.view.drawables.DrawableWrapper
 
 /**
   * System to draw all the entity
@@ -26,5 +27,16 @@ class DrawSystem(levelContext: LevelContext, priority: Int) extends System[Drawa
 
   override def getGroupProperty(): Class[_ <: Property] = classOf[DrawableProperty]
 
-  override def update(): Unit = ???
+  override def update(): Unit = levelContext.drawEntities(getPlayerEntity, getEntities)
+
+  private def getPlayerEntity: Option[DrawableWrapper] =
+    player filter(p => p.getVisibleComponent.isVisible()) map(p => drawablePropertyToDrawableWrapper(p))
+
+  private def getEntities: List[DrawableWrapper] =
+    entities filter(e => e.getVisibleComponent.isVisible()) map(e => drawablePropertyToDrawableWrapper(e)) toList
+
+  private def drawablePropertyToDrawableWrapper(entity: DrawableProperty): DrawableWrapper =
+    DrawableWrapper(entity.getPositionComponent.point,
+                    entity.getDimensionComponent.radius,
+                    entity.getTypeComponent.typeEntity)
 }
