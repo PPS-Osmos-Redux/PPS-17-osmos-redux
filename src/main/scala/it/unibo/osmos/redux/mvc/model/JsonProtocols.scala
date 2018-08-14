88b02a292e735e57a3027dca11342f2bcd3df2c0
Andrea Procucci <Andrea Procucci>
2018-08-14 10:35:04 +0200

    Added level data structure + json protocol for level conversion

--- src/main/scala/it/unibo/osmos/redux/mvc/model/JsonProtocols.scala
+++ src/main/scala/it/unibo/osmos/redux/mvc/model/JsonProtocols.scala
@@ -4,13 +4,13 @@ import DefaultJsonProtocol._
 import it.unibo.osmos.redux.ecs.components._
 import it.unibo.osmos.redux.ecs.entities.{CellEntity, PlayerCellEntity}
 import it.unibo.osmos.redux.utils.Point
-import spray.json.{DeserializationException, JsBoolean, JsNumber, JsObject, JsString, JsValue, RootJsonFormat}
+import org.apache.commons.lang3.SerializationException
 
 object JsonProtocols {
   implicit object AccelerationFormatter extends RootJsonFormat[AccelerationComponent] {
-    def write(c: AccelerationComponent) = JsObject(
-      "accelerationX" -> JsNumber(c.accelerationX),
-      "accelerationY" -> JsNumber(c.accelerationY)
+    def write(acceleration: AccelerationComponent) = JsObject(
+      "accelerationX" -> JsNumber(acceleration.accelerationX),
+      "accelerationY" -> JsNumber(acceleration.accelerationY)
     )
     def read(value: JsValue): AccelerationComponent = {
       value.asJsObject.getFields("accelerationX", "accelerationY") match {
@@ -22,7 +22,8 @@ object JsonProtocols {
   }
 
   implicit object CollidableFormatter extends RootJsonFormat[CollidableComponent] {
-    def write(c: CollidableComponent) = JsObject("collidable" -> JsBoolean(c.isCollidable()))
+    def write(collidable: CollidableComponent) =
+      JsObject("collidable" -> JsBoolean(collidable.isCollidable()))
     def read(value: JsValue): CollidableComponent = {
       value.asJsObject.getFields("collidable") match {
         case Seq(JsBoolean(collidable)) => CollidableComponent.apply(collidable)
@@ -32,7 +33,7 @@ object JsonProtocols {
   }
 
   implicit object VisibleFormatter extends RootJsonFormat[VisibleComponent] {
-    def write(c: VisibleComponent) = JsObject("visible" -> JsBoolean(c.isVisible()))
+    def write(visible: VisibleComponent) = JsObject("visible" -> JsBoolean(visible.isVisible()))
     def read(value: JsValue): VisibleComponent = {
       value.asJsObject.getFields("visible") match {
         case Seq(JsBoolean(visible)) => VisibleComponent.apply(visible)
@@ -42,7 +43,7 @@ object JsonProtocols {
   }
 
   implicit object DimensionFormatter extends RootJsonFormat[DimensionComponent] {
-    def write(c: DimensionComponent) = JsObject("radius" -> JsNumber(c.radius))
+    def write(dimension: DimensionComponent) = JsObject("radius" -> JsNumber(dimension.radius))
     def read(value: JsValue): DimensionComponent = {
       value.asJsObject.getFields("radius") match {
         case Seq(JsNumber(radius)) => DimensionComponent.apply(radius.toDouble)
@@ -52,7 +53,7 @@ object JsonProtocols {
   }
 
   implicit object PointFormatter extends RootJsonFormat[Point] {
-    def write(c: Point) = JsObject("x" -> JsNumber(c.x), "y" -> JsNumber(c.y))
+    def write(point: Point) = JsObject("x" -> JsNumber(point.x), "y" -> JsNumber(point.y))
     def read(value: JsValue): Point = {
       value.asJsObject.getFields("x","y") match {
         case Seq(JsNumber(x), JsNumber(y)) => Point.apply(x.toDouble,y.toDouble)
@@ -62,7 +63,7 @@ object JsonProtocols {
   }
 
   implicit object PositionFormatter extends RootJsonFormat[PositionComponent] {
-    def write(c: PositionComponent) = JsObject("point" -> c.point.toJson)
+    def write(position: PositionComponent) = JsObject("point" -> position.point.toJson)
     def read(value: JsValue): PositionComponent = {
       value.asJsObject.getFields("point") match {
         case Seq(point) => PositionComponent.apply(point.convertTo[Point])
@@ -72,8 +73,8 @@ object JsonProtocols {
   }
 
   implicit object SpeedFormatter extends RootJsonFormat[SpeedComponent] {
-    def write(c: SpeedComponent) =
-      JsObject("speedX" -> JsNumber(c.speedX), "speedY" -> JsNumber(c.speedY))
+    def write(speed: SpeedComponent) =
+      JsObject("speedX" -> JsNumber(speed.speedX), "speedY" -> JsNumber(speed.speedY))
     def read(value: JsValue): SpeedComponent = {
       value.asJsObject.getFields("speedX","speedY") match {
         case Seq(JsNumber(speedX), JsNumber(speedY)) =>
@@ -84,7 +85,8 @@ object JsonProtocols {
   }
 
   implicit object EntityTypeFormatter extends RootJsonFormat[TypeComponent] {
-    def write(c: TypeComponent) = JsObject("entity_type" ->  JsString(c.typeEntity.toString))
+    def write(entityType: TypeComponent) =
+      JsObject("entity_type" ->  JsString(entityType.typeEntity.toString))
     def read(value: JsValue): TypeComponent = {
       value.asJsObject.getFields("entity_type") match {
         case Seq(JsString(entityType)) => TypeComponent.apply(EntityType.withName(entityType))
@@ -94,14 +96,14 @@ object JsonProtocols {
   }
 
   implicit object CellEntityFormatter extends RootJsonFormat[CellEntity] {
-    def write(c: CellEntity) = JsObject(
-      "acceleration" -> c.getAccelerationComponent.toJson,
-      "collidable" -> c.getCollidableComponent.toJson,
-      "dimension" -> c.getDimensionComponent.toJson,
-      "position" -> c.getPositionComponent.toJson,
-      "speed" -> c.getSpeedComponent.toJson,
-      "visible" -> c.getVisibleComponent.toJson,
-      "typeEntity" -> c.getTypeComponent.toJson)
+    def write(cellEntity: CellEntity) = JsObject(
+      "acceleration" -> cellEntity.getAccelerationComponent.toJson,
+      "collidable" -> cellEntity.getCollidableComponent.toJson,
+      "dimension" -> cellEntity.getDimensionComponent.toJson,
+      "position" -> cellEntity.getPositionComponent.toJson,
+      "speed" -> cellEntity.getSpeedComponent.toJson,
+      "visible" -> cellEntity.getVisibleComponent.toJson,
+      "typeEntity" -> cellEntity.getTypeComponent.toJson)
     def read(value: JsValue): CellEntity = {
       value.asJsObject.getFields("acceleration",
         "collidable",
@@ -124,14 +126,14 @@ object JsonProtocols {
   }
 
   implicit object PlayerCellEntityFormatter extends RootJsonFormat[PlayerCellEntity] {
-    def write(c: PlayerCellEntity) = JsObject(
-      "acceleration" -> c.getAccelerationComponent.toJson,
-      "collidable" -> c.getCollidableComponent.toJson,
-      "dimension" -> c.getDimensionComponent.toJson,
-      "position" -> c.getPositionComponent.toJson,
-      "speed" -> c.getSpeedComponent.toJson,
-      "visible" -> c.getVisibleComponent.toJson,
-      "typeEntity" -> c.getTypeComponent.toJson)
+    def write(playerCell: PlayerCellEntity) = JsObject(
+      "acceleration" -> playerCell.getAccelerationComponent.toJson,
+      "collidable" -> playerCell.getCollidableComponent.toJson,
+      "dimension" -> playerCell.getDimensionComponent.toJson,
+      "position" -> playerCell.getPositionComponent.toJson,
+      "speed" -> playerCell.getSpeedComponent.toJson,
+      "visible" -> playerCell.getVisibleComponent.toJson,
+      "typeEntity" -> playerCell.getTypeComponent.toJson)
     def read(value: JsValue): PlayerCellEntity = {
       value.asJsObject.getFields("acceleration",
         "collidable",
@@ -152,4 +154,49 @@ object JsonProtocols {
       }
     }
   }
+
+  implicit object MapShapeFormatter extends RootJsonFormat[MapShape] {
+    def write(mapShape: MapShape): JsObject = mapShape match {
+      case mapShape:MapShape.Rectangle => JsObject("mapShape" -> JsString(mapShape.mapShape),
+        "height" -> JsNumber(mapShape.height),
+        "base" -> JsNumber(mapShape.base))
+      case mapShape:MapShape.Circle => JsObject("mapShape" -> JsString(mapShape.mapShape),
+        "radius" -> JsNumber(mapShape.radius))
+      case _ => throw new SerializationException("Shape " + mapShape.mapShape + " not managed!")
+    }
+
+    def read(value: JsValue): MapShape = {
+      value.asJsObject.getFields("mapShape", "height", "base", "radius") match {
+        case Seq(JsString(MapShape.rectangle), JsNumber(height), JsNumber(base)) =>
+          MapShape.Rectangle(height.toDouble, base.toDouble)
+        case Seq(JsString(MapShape.circle), JsNumber(radius)) => MapShape.Circle(radius.toDouble)
+        case _ => throw DeserializationException("Map shape expected")
+      }
+    }
+  }
+
+  implicit object LevelMapFormatter extends RootJsonFormat[LevelMap] {
+    def write(levelMap: LevelMap) = JsObject(
+      "mapShape" -> levelMap.mapShape.toJson,
+      "collisionRule" -> JsString(levelMap.collisionRule.toString))
+    def read(value: JsValue): LevelMap = {
+      value.asJsObject.getFields("mapShape","collisionRule") match {
+        case Seq(mapShape, JsString(collisionRule)) =>
+          LevelMap(mapShape.convertTo[MapShape], CollisionRules.withName(collisionRule))
+        case _ => throw DeserializationException("Level map expected")
+      }
+    }
+  }
+
+  implicit object VictoryRuleFormatter extends RootJsonFormat[VictoryRules.Value] {
+    def write(vicRule: VictoryRules.Value) = JsObject("victoryRule" -> JsString(vicRule.toString))
+    def read(value: JsValue): VictoryRules.Value = {
+      value.asJsObject.getFields("victoryRule") match {
+        case Seq(JsString(victoryRule)) => VictoryRules.withName(victoryRule)
+        case _ => throw DeserializationException("Victory rule expected expected")
+      }
+    }
+  }
+
+  implicit val levelFormatter:RootJsonFormat[Level] = jsonFormat5(Level)
 }
--- src/main/scala/it/unibo/osmos/redux/mvc/model/Level.scala
+++ src/main/scala/it/unibo/osmos/redux/mvc/model/Level.scala
@@ -1,28 +1,9 @@
 package it.unibo.osmos.redux.mvc.model
 import spray.json._
 import DefaultJsonProtocol._
-import it.unibo.osmos.redux.ecs.entities.CellEntity
-import org.apache.commons.lang3.SerializationException
-
-object JsonProtocol2 {
-
-  implicit object MapShapeFormatter extends RootJsonFormat[MapShape] {
-    def write(c: MapShape): JsObject = c match {
-      case c:Rectangle => JsObject("height" -> JsNumber(c.height),"base" -> JsNumber(c.base))
-      case c:Circle => JsObject("radius" -> JsNumber(c.radius))
-      case _ => throw new SerializationException("Shape " + c.getClass.getName + " not managed!")
-    }
-
-    def read(value: JsValue): MapShape = {
-      value.asJsObject.getFields("height", "base", "radius") match {
-        case Seq(JsNumber(height), JsNumber(base)) => Rectangle(height.toDouble, base.toDouble)
-        case Seq(JsNumber(radius)) => Circle(radius.toDouble)
-        case _ => throw DeserializationException("Map shape expected")
-      }
-    }
-  }
-
-}
+import it.unibo.osmos.redux.ecs.components._
+import it.unibo.osmos.redux.ecs.entities._
+import it.unibo.osmos.redux.utils.Point
 
 object CollisionRules extends Enumeration {
   val bouncing: CollisionRules.Value = Value("Bouncing")
@@ -33,16 +14,55 @@ object VictoryRules extends Enumeration {
   val becomeTheBiggest: VictoryRules.Value = Value("Become_the_biggest")
 }
 
-sealed trait MapShape
-case class Rectangle(height:Double, base:Double) extends MapShape
-case class Circle(radius:Double) extends MapShape
-case class Quad(d:Double, y:Double) extends MapShape
+sealed trait MapShape {
+  val mapShape:String
+}
 
+object MapShape {
+  val rectangle:String = "RECTANGLE"
+  val circle:String = "CIRCLE"
+  case class Rectangle(height:Double, base:Double) extends MapShape {
+    override val mapShape: String = MapShape.rectangle
+  }
+  case class Circle(radius:Double) extends MapShape {
+    override val mapShape: String = MapShape.circle
+  }
+}
 
 case class LevelMap(mapShape:MapShape, collisionRule:CollisionRules.Value)
 
 case class Level(levelId:Int,
                  levelMap:LevelMap,
                  entities:List[CellEntity],
-                 winningRule:VictoryRules.Value,
+                 victoryRule:VictoryRules.Value,
                  isSimulation:Boolean)
+
+
+object deus extends App {
+  val a = AccelerationComponent(1, 1)
+  val c = CollidableComponent(true)
+  val d = DimensionComponent(5)
+  val p = PositionComponent(Point(0, 0))
+  val s = SpeedComponent(4, 0)
+  val v = VisibleComponent(true)
+  val et = TypeComponent(EntityType.Material)
+  val ce = CellEntity(a, c, d, p, s, v, et)
+  val pce = PlayerCellEntity(a,c,d,p,s,v, et)
+  val listCell:List[CellEntity] = List(ce, pce)
+
+  val rec:MapShape = MapShape.Rectangle(10.1,5.6)
+  val circ:MapShape = MapShape.Circle(5.7)
+  val listShape:List[MapShape] = List(rec, circ)
+
+  val levelMap:LevelMap = LevelMap(rec,CollisionRules.bouncing)
+
+  val level:Level = Level(1, levelMap, listCell, VictoryRules.becomeTheBiggest, isSimulation = false)
+
+  import it.unibo.osmos.redux.mvc.model.JsonProtocols._
+
+  val json = level.toJson
+  println(json)
+  val fromJson = json.convertTo[Level]
+  print(fromJson)
+
+}
