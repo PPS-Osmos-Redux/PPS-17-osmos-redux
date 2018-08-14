20e5b3a0fbad0e85691c3241d3f05dbc9667c142
DavideGia <davide.giacomini2@studio.unibo.it>
2018-08-14 15:21:16 +0200

    Refactor: CollidableProperty must have access to entity acceleration

--- src/main/scala/it/unibo/osmos/redux/ecs/entities/CollidableProperty.scala
+++ src/main/scala/it/unibo/osmos/redux/ecs/entities/CollidableProperty.scala
@@ -3,6 +3,6 @@ package it.unibo.osmos.redux.ecs.entities
 /**
   * Trait representing the properties needed by an entity to be collidable
   */
-trait CollidableProperty extends Position with Dimension with Speed with Collidable {
+trait CollidableProperty extends Position with Dimension with Speed with Collidable with Acceleration {
 
 }
