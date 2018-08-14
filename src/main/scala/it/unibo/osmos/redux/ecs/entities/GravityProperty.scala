689fa44a618552970b54b256200c2791f4623f98
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-14 17:07:33 +0200

    add group of property for gravity entity

---
+++ src/main/scala/it/unibo/osmos/redux/ecs/entities/GravityProperty.scala
@@ -0,0 +1,6 @@
+package it.unibo.osmos.redux.ecs.entities
+
+/**
+  * Trait representing the properties needed by an entity to exercise gravitational forces
+  */
+trait GravityProperty extends Position with Mass with Type{}
