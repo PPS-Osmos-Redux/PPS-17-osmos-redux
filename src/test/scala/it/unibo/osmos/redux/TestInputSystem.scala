1edd4a9bec6a8060138e12ee87f16a1d59ad487f
DavideGia <davide.giacomini2@studio.unibo.it>
2018-08-14 23:19:10 +0200

    Fixed: TestInputSystem test class

--- src/test/scala/it/unibo/osmos/redux/TestInputSystem.scala
+++ src/test/scala/it/unibo/osmos/redux/TestInputSystem.scala
@@ -3,9 +3,9 @@ package it.unibo.osmos.redux
 import it.unibo.osmos.redux.ecs.components._
 import it.unibo.osmos.redux.ecs.entities.{CellEntity, EntityManager, PlayerCellEntity}
 import it.unibo.osmos.redux.ecs.systems.InputSystem
-import it.unibo.osmos.redux.mvc.view.events.MouseEventWrapper
 import it.unibo.osmos.redux.mvc.view.levels.LevelContext
-import it.unibo.osmos.redux.utils.{InputEventQueue, Point}
+import it.unibo.osmos.redux.mvc.view.events.MouseEventWrapper
+import it.unibo.osmos.redux.utils.{InputEventQueue, MathUtils, Point}
 import org.scalatest.FunSuite
 
 class TestInputSystem extends FunSuite {
@@ -18,7 +18,7 @@ class TestInputSystem extends FunSuite {
   val visibility = Seq(VisibleComponent(true), VisibleComponent(false), VisibleComponent(false))
   val typeEntity = Seq(TypeComponent(EntityType.Material), TypeComponent(EntityType.Material), TypeComponent(EntityType.Material))
 
-  val dummyEvent = MouseEventWrapper(Point(1.0, 1.0))
+  val dummyEvent = MouseEventWrapper(Point(157,104))
 
   test("InputSystem priority must match the one passed at his constructor") {
     val priority = 0
@@ -39,19 +39,19 @@ class TestInputSystem extends FunSuite {
     val pce = PlayerCellEntity(acceleration(0), collidable(0), dimension(0), position(0), speed(0), visibility(0), typeEntity(0), SpawnerComponent(false))
     EntityManager.add(pce)
 
-    //save original acceleration value
-    val originalAccel = AccelerationComponent(acceleration(0).accelerationX, acceleration(0).accelerationY)
+    //prepare list of events to apply
+    val events = List(dummyEvent,  MouseEventWrapper(Point(200,194)), MouseEventWrapper(Point(314,44)))
 
-    //add mouse event to Input event stack
-    InputEventQueue.enqueue(dummyEvent)
-    InputEventQueue.enqueue(dummyEvent)
-    InputEventQueue.enqueue(dummyEvent)
+    //add mouse events to Input event stack
+    InputEventQueue.enqueue(events: _*)
+
+    //pre-compute expected values
+    val expectedAccel = computeExpectedAcceleration(system, pce, events: _*)
 
     //call system update
     system.update()
 
-    assert(pce.getAccelerationComponent.accelerationX == (originalAccel.accelerationX - (system.accelCoefficient*3)) &&
-      pce.getAccelerationComponent.accelerationY == (originalAccel.accelerationY - (system.accelCoefficient*3)))
+    assert(pce.getAccelerationComponent == expectedAccel)
   }
 
   test("InputSystem should update only entities with input property") {
@@ -70,8 +70,10 @@ class TestInputSystem extends FunSuite {
     EntityManager.add(ce)
 
     //save original acceleration value
-    val originalAccel0 = AccelerationComponent(acceleration(0).accelerationX, acceleration(0).accelerationY)
-    val originalAccel1 = AccelerationComponent(acceleration(1).accelerationX, acceleration(1).accelerationY)
+    val originalAccel = AccelerationComponent(acceleration(1).accelerationX, acceleration(1).accelerationY)
+
+    //pre-compute expected values
+    val expectedAccel = computeExpectedAcceleration(system, pce, dummyEvent)
 
     //add mouse event to Input event stack
     InputEventQueue.enqueue(dummyEvent)
@@ -79,8 +81,19 @@ class TestInputSystem extends FunSuite {
     //call system update
     system.update()
 
-    assert(pce.getAccelerationComponent.accelerationX == (originalAccel0.accelerationX - system.accelCoefficient) &&
-      pce.getAccelerationComponent.accelerationY == (originalAccel0.accelerationX - system.accelCoefficient) &&
-      ce.getAccelerationComponent == originalAccel1)
+    assert(pce.getAccelerationComponent == expectedAccel && ce.getAccelerationComponent == originalAccel)
+  }
+
+  def computeExpectedAcceleration(system: InputSystem, entity: PlayerCellEntity, events: MouseEventWrapper*): AccelerationComponent = {
+    val pos = entity.getPositionComponent
+    val accel = entity.getAccelerationComponent
+
+    var newAccel = AccelerationComponent(accel.accelerationX, accel.accelerationY)
+    events foreach (ev => {
+      val p = MathUtils.normalizePoint(Point(pos.point.x - ev.point.x, pos.point.y - ev.point.y))
+      newAccel = AccelerationComponent(newAccel.accelerationX + p.x * system.accelCoefficient,
+        newAccel.accelerationY + p.y * system.accelCoefficient)
+    })
+    newAccel
   }
 }
