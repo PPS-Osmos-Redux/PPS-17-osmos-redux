e251e305b09d297244ce1644bc29c08635e19f81
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-15 11:39:17 +0200

    add first test for gravity system

---
+++ src/test/scala/it/unibo/osmos/redux/TestGravitySystem.scala
@@ -0,0 +1,41 @@
+package it.unibo.osmos.redux
+
+import it.unibo.osmos.redux.ecs.components._
+import it.unibo.osmos.redux.ecs.entities.{CellEntity, EntityManager, GravityCellEntity}
+import it.unibo.osmos.redux.ecs.systems.GravitySystem
+import it.unibo.osmos.redux.utils.Point
+import org.scalatest.{BeforeAndAfter, FunSuite}
+import org.scalactic.Tolerance._
+
+class TestGravitySystem extends FunSuite with BeforeAndAfter{
+
+  val TOLERANCE = 0.03
+  val acceleration = AccelerationComponent(1, 1)
+  val collidable = CollidableComponent(true)
+  val speed = SpeedComponent(4, 0)
+  val dimension = DimensionComponent(3)
+  val position = PositionComponent(Point(0, 0))
+  val visible = VisibleComponent(true)
+  val typeEntity = TypeComponent(EntityType.Attractive)
+  val specificWeight = SpecificWeightComponent(1.5)
+
+  val dimension1 = DimensionComponent(5)
+  val position1 = PositionComponent(Point(3, 4))
+
+  test("check mass calculation") {
+    val gravityCellEntity = GravityCellEntity(acceleration,collidable,dimension,position,speed,visible,typeEntity,specificWeight)
+    assert(gravityCellEntity.getMassComponent.mass === 42.39 +- TOLERANCE)
+  }
+
+  test("Acceleration of CellEntity should not change without GravityCellEntity") {
+    val cellEntity = CellEntity(acceleration,collidable,dimension,position,speed,visible,typeEntity)
+    val gravitySystem = GravitySystem(0)
+    EntityManager.add(cellEntity)
+
+    val originalAcceleration = AccelerationComponent(cellEntity.getAccelerationComponent.accelerationX, cellEntity.getAccelerationComponent.accelerationY)
+    gravitySystem.update()
+    assert(cellEntity.getAccelerationComponent.accelerationX === originalAcceleration.accelerationX)
+  }
+
+
+}
