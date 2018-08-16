e706a6a4a4090c2ce117649d7a4c890ab66ffe38
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-16 15:56:19 +0200

    fix test

--- src/test/scala/it/unibo/osmos/redux/TestSystemWithTwoTypeOfEntity.scala
+++ src/test/scala/it/unibo/osmos/redux/TestSystemWithTwoTypeOfEntity.scala
@@ -43,14 +43,10 @@ class TestSystemWithTwoTypeOfEntity extends FunSuite with BeforeAndAfter {
 
   test("After add one entity of first type, the system have one entity of first type"){
     val fakeSystem = FakeSystemWithTwoTypeOfEntity(1)
-    EntityManager.add(PlayerCellEntity(acceleration, collidable, dimension, position, speed, visible, typeEntity))
     val ce = CellEntity(acceleration, collidable, dimension, position, speed, visible, typeEntity)
     EntityManager.add(ce)
-    println(fakeSystem.getEntitiesOfFirstType.size)
-    println(fakeSystem.getEntitiesOfSecondType.size)
-    println(fakeSystem.getEntitiesOfSecondType)
-    assert(fakeSystem.getEntitiesOfFirstType.size == 2)
-    assert(fakeSystem.getEntitiesOfSecondType.size == 1)
+    assert(fakeSystem.getEntitiesOfFirstType.size == 1)
+    assert(fakeSystem.getEntitiesOfSecondType.isEmpty)
   }
 
   test("An entity of both types is found in both lists"){
