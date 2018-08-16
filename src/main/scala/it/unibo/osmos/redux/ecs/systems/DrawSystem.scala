051162e74aa55b572a5a377ce4dca019227dbc37
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-16 17:24:27 +0200

    drawSystem now extends from AbstractSystemWithTwoTypeOfEntity

--- src/main/scala/it/unibo/osmos/redux/ecs/systems/DrawSystem.scala
+++ src/main/scala/it/unibo/osmos/redux/ecs/systems/DrawSystem.scala
@@ -1,6 +1,5 @@
 package it.unibo.osmos.redux.ecs.systems
 
-import it.unibo.osmos.redux.ecs.entities.EMEvents.{EntityCreated, EntityDeleted}
 import it.unibo.osmos.redux.ecs.entities._
 import it.unibo.osmos.redux.mvc.view.levels.LevelContext
 import it.unibo.osmos.redux.mvc.view.drawables.DrawableWrapper
@@ -8,32 +7,20 @@ import it.unibo.osmos.redux.mvc.view.drawables.DrawableWrapper
 /**
   * System to draw all the entity
   * @param levelContext levelContext for communicate the entities to the view
-  * @param priority system priority
   */
-case class DrawSystem(levelContext: LevelContext) extends AbstractSystem[DrawableProperty] {
-
-  private var player: Option[DrawableProperty] = None
-
-  override def notify(event: EMEvents.EntityManagerEvent): Unit = {
-    if (event.entity.isInstanceOf[PlayerCellEntity]) {
-      event match {
-        case event: EntityCreated => player = Some(event.entity.asInstanceOf[DrawableProperty])
-        case _: EntityDeleted => player = None
-      }
-    } else {
-      super.notify(event)
-    }
-  }
+case class DrawSystem(levelContext: LevelContext) extends AbstractSystemWithTwoTypeOfEntity[DrawableProperty, PlayerCellEntity] {
 
   override def getGroupProperty: Class[_ <: Property] = classOf[DrawableProperty]
+
+  override protected def getGroupPropertySecondType: Class[_ <: Property] = classOf[PlayerCellEntity]
 
   override def update(): Unit = levelContext.drawEntities(getPlayerEntity, getEntities)
 
   private def getPlayerEntity: Option[DrawableWrapper] =
-    player filter(p => p.getVisibleComponent.isVisible()) map(p => drawablePropertyToDrawableWrapper(p))
+    entitiesSecondType find (p => p.getVisibleComponent.isVisible())  map(p => drawablePropertyToDrawableWrapper(p))
 
   private def getEntities: List[DrawableWrapper] =
-    entities.filter(e => e.getVisibleComponent.isVisible()).map(e => drawablePropertyToDrawableWrapper(e)).toList
+    entities filter(e => e.getVisibleComponent.isVisible()) map(e => drawablePropertyToDrawableWrapper(e)) toList
 
   private def drawablePropertyToDrawableWrapper(entity: DrawableProperty): DrawableWrapper =
     DrawableWrapper(entity.getPositionComponent.point,
