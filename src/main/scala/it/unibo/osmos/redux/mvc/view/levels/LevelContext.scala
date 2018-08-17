549f6d769a4cef4accd596a05609f8713ac9b9bc
Marco Galassi <marco.galassi6@studio.unibo.it>
2018-08-17 10:48:10 +0200

    Added new EntitiesDrawer trait to decompose LevelContext features

--- src/main/scala/it/unibo/osmos/redux/mvc/view/levels/LevelContext.scala
+++ src/main/scala/it/unibo/osmos/redux/mvc/view/levels/LevelContext.scala
@@ -1,23 +1,16 @@
 package it.unibo.osmos.redux.mvc.view.levels
 
-import it.unibo.osmos.redux.mvc.view.drawables.DrawableWrapper
+import it.unibo.osmos.redux.mvc.view.drawables.{DrawableWrapper, EntitiesDrawer}
 import it.unibo.osmos.redux.mvc.view.events.{EventWrapperListener, EventWrapperSource, MouseEventWrapper}
 
 /**
   * Trait modelling the context of a level
   */
-trait LevelContext extends EventWrapperSource[MouseEventWrapper]{
+trait LevelContext extends EventWrapperSource[MouseEventWrapper] with EntitiesDrawer {
   /**
     * Called once at the beginning at the level. Manages the context creation
     */
   def setupLevel()
-
-  /**
-    * Called once per frame. Manages the entities that must be drawn
-    * @param playerEntity the player entity. It may be empty
-    * @param entities the other entities
-    */
-  def drawEntities(playerEntity: Option[DrawableWrapper], entities: Seq[DrawableWrapper])
 }
 
 object LevelContext {
