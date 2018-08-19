e028ddfa64257b57a63613c984b4e9e5c07a3c3c
Giacomo <giacomo.venturini2@studio.unibo.it>
2018-08-20 00:52:27 +0200

    remove old implementation leftovers, improve MovementSystem using Vector's methods

--- src/main/scala/it/unibo/osmos/redux/ecs/components/AccelerationComponent.scala
+++ src/main/scala/it/unibo/osmos/redux/ecs/components/AccelerationComponent.scala
@@ -2,55 +2,23 @@ package it.unibo.osmos.redux.ecs.components
 
 import it.unibo.osmos.redux.utils
 
-/**
-  * Component of the acceleration vector
-  */
+/** Component of the acceleration vector */
 trait AccelerationComponent extends VectorComponent {
 
-  /**
-    * Getter. Return the acceleration of the x coordinate
-    *
-    * @return the acceleration
-    */
-  //def x: Double
-
-  /**
-    * Getter. Return the acceleration of the y coordinate
-    *
-    * @return the acceleration
-    */
-  //def y: Double
-
-  /**
-    * Setter. Set the new acceleration of the x coordinate
-    *
-    * @param acceleration the new acceleration
-    */
-  //def x_(acceleration: Double): Unit
-
-  /**
-    * Setter. Set the new acceleration of the y coordinate
-    *
-    * @param acceleration the new acceleration
-    */
-  //def y_(acceleration: Double): Unit
+  /** Resets this component vector's components to 0 */
+  def reset(): Unit
 }
 
 object AccelerationComponent {
   def apply(accelerationX: Double, accelerationY: Double): AccelerationComponent = AccelerationComponentImpl(utils.Vector(accelerationX, accelerationY))
 
   private case class AccelerationComponentImpl(var _speedVector: utils.Vector) extends AccelerationComponent {
-    /*override def x: Double = _accelerationX
-
-    override def y: Double = _accelerationY
-
-    override def x_(acceleration: Double): Unit = _accelerationX = acceleration
-
-    override def y_(acceleration: Double): Unit = _accelerationY = acceleration*/
 
     override def vector: utils.Vector = _speedVector
 
     override def vector_(vector: utils.Vector): Unit = _speedVector = vector
+
+    override def reset(): Unit = vector_(utils.Vector(0, 0))
   }
 
 }
