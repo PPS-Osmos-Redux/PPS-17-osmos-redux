24bf82c19fb575d6c0c43827cfc42057f4c51bd2
Marco Galassi <marco.galassi6@studio.unibo.it>
2018-08-22 00:49:28 +0200

    Added scroll events to resize entities; testing colors and shapes

--- src/main/scala/it/unibo/osmos/redux/mvc/view/scenes/EditorScene.scala
+++ src/main/scala/it/unibo/osmos/redux/mvc/view/scenes/EditorScene.scala
@@ -3,7 +3,11 @@ package it.unibo.osmos.redux.mvc.view.scenes
 import it.unibo.osmos.redux.ecs.components.EntityType
 import it.unibo.osmos.redux.mvc.view.components.TitledComboBox
 import it.unibo.osmos.redux.mvc.view.loaders.ImageLoader
+import javafx.scene.paint.ImagePattern
+import scalafx.scene.effect.ColorAdjust
 import scalafx.scene.image.ImageView
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Circle
 import scalafx.stage.Stage
 
 import scala.tools.nsc.doc.model.Entity
@@ -28,6 +32,28 @@ class EditorScene (override val parentStage: Stage, val listener: EditorSceneLis
     */
   private var entityType: EntityType.Value = EntityType.Matter
   private val entityComboBox = new TitledComboBox[EntityType.Value]("Entity Type", EntityType.values.toSeq, et => {entityType = et; println("val: " + entityType);})
+
+  onMouseClicked = click => {
+    val entity: Circle = new Circle(){
+      fill.value = new ImagePattern(ImageLoader.getImage("/textures/cell.png"))
+
+      centerX = click.getX
+      centerY = click.getY
+      radius = 100
+
+      onScroll = scroll => radius = radius.value + (scroll.getDeltaY/10) min 200 max 10
+
+      entityType match {
+        case EntityType.Matter => effect = new ColorAdjust(0.0, 0, 0 ,0)
+        case EntityType.AntiMatter => effect = new ColorAdjust(0.5, 0, 0 ,0)
+        case EntityType.Attractive => effect = new ColorAdjust(0.3, 0.5, -0.8 ,0)
+        case EntityType.Repulse => effect = new ColorAdjust(-0.3, 0, 0 ,0)
+        case EntityType.Sentient => effect = new ColorAdjust(-0.7, 1,0.3 ,0)
+      }
+    }
+
+    content.add(entity)
+  }
 
   content = Seq(background, entityComboBox)
 
