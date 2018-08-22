24b6f6e527a0cf6584eedff1e661c76d55ae2c86
Marco Galassi <marco.galassi6@studio.unibo.it>
2018-08-23 00:52:46 +0200

    Updated EditorScene with new user input parsing, observables, a new way to manage copy and insertion

--- src/main/scala/it/unibo/osmos/redux/mvc/view/scenes/EditorScene.scala
+++ src/main/scala/it/unibo/osmos/redux/mvc/view/scenes/EditorScene.scala
@@ -1,15 +1,20 @@
 package it.unibo.osmos.redux.mvc.view.scenes
 
 import it.unibo.osmos.redux.ecs.components.EntityType
-import it.unibo.osmos.redux.mvc.view.components.editor.TitledComboBox
+import it.unibo.osmos.redux.mvc.view.components.editor.{CellEntityBuilder, TitledComboBox}
 import it.unibo.osmos.redux.mvc.view.loaders.ImageLoader
 import javafx.scene.paint.ImagePattern
-import scalafx.scene.effect.ColorAdjust
+import scalafx.beans.Observable
+import scalafx.beans.property.ObjectProperty
+import scalafx.beans.value.ObservableValue
+import scalafx.scene.effect.{ColorAdjust, Effect}
 import scalafx.scene.image.ImageView
+import scalafx.scene.layout.VBox
 import scalafx.scene.paint.Color
-import scalafx.scene.shape.Circle
+import scalafx.scene.shape.{Circle, Shape}
 import scalafx.stage.Stage
 
+import scala.collection.mutable
 import scala.tools.nsc.doc.model.Entity
 
 /**
@@ -30,32 +35,75 @@ class EditorScene (override val parentStage: Stage, val listener: EditorSceneLis
   /**
     * Entity Type
     */
-  private var entityType: EntityType.Value = EntityType.Matter
-  private val entityComboBox = new TitledComboBox[EntityType.Value]("Entity Type", EntityType.values.toSeq, et => {entityType = et; println("val: " + entityType);})
+  private var entityType: ObjectProperty[EntityType.Value] = ObjectProperty(EntityType.Matter)
 
-  onMouseClicked = click => {
-    val entity: Circle = new Circle(){
-      fill.value = new ImagePattern(ImageLoader.getImage("/textures/cell.png"))
+  private val entityComboBox = new TitledComboBox[EntityType.Value]("Entity Type", EntityType.values.toSeq, et => {
+    entityType.value = et
+    println("val: " + entityType)
 
-      centerX = click.getX
-      centerY = click.getY
-      radius = 100
+  })
 
-      onScroll = scroll => radius = radius.value + (scroll.getDeltaY/10) min 200 max 10
+  private val cellEntityBuilder = new CellEntityBuilder
 
-      entityType match {
+  val verticalContainer: VBox = new VBox(10.0, entityComboBox, cellEntityBuilder)
+
+
+  val entityPlaceholder: Circle = new Circle() {
+    fill.value = new ImagePattern(ImageLoader.getImage("/textures/menu_level_1.png"))
+    radius = 100
+
+    onScroll = scroll => radius = radius.value + (scroll.getDeltaY/10) min 200 max 10
+
+    entityType.onChange(entityType.value match {
         case EntityType.Matter => effect = new ColorAdjust(0.0, 0, 0 ,0)
         case EntityType.AntiMatter => effect = new ColorAdjust(0.5, 0, 0 ,0)
         case EntityType.Attractive => effect = new ColorAdjust(0.3, 0.5, -0.8 ,0)
         case EntityType.Repulse => effect = new ColorAdjust(-0.3, 0, 0 ,0)
         case EntityType.Sentient => effect = new ColorAdjust(-0.7, 1,0.3 ,0)
-      }
-    }
+        case _ =>
+      })
 
-    content.add(entity)
   }
 
-  content = Seq(background, entityComboBox)
+  var currentPlaceholder: Shape = entityPlaceholder
+
+
+  onKeyPressed = key => {
+    currentPlaceholder.visible = key.isControlDown
+  }
+
+  onMouseMoved = e => {
+    currentPlaceholder match {
+      case c:Circle => {
+        c.centerX.value = e.getX
+        c.centerY.value = e.getY
+        cellEntityBuilder.x.value = e.getX
+        cellEntityBuilder.y.value = e.getY
+      }
+      case _ =>
+    }
+
+  }
+
+  onMouseClicked = _ => {
+    currentPlaceholder match {
+      case c:Circle => {
+        editorElements += new Circle {
+          fill.value_=(c.fill.value)
+          centerX = c.centerX.value
+          centerY = c.centerY.value
+          radius = c.radius.value
+          effect.value_=(c.effect.value)
+        }
+      }
+      case _ =>
+    }
+
+    content = editorElements
+  }
+
+  val editorElements = mutable.MutableList(background, verticalContainer, currentPlaceholder)
+  content = editorElements
 
 }
 
