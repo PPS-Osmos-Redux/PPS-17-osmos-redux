dcc49f0d3b47244f13d63c92c41fa22e46432d16
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-23 17:52:44 +0200

    add comments

--- src/main/scala/it/unibo/osmos/redux/ecs/systems/SentientSystem.scala
+++ src/main/scala/it/unibo/osmos/redux/ecs/systems/SentientSystem.scala
@@ -25,6 +25,11 @@ case class SentientSystem() extends AbstractSystemWithTwoTypeOfEntity[SentientPr
     runAwayFromEnemies(sentient, findEnemies(sentient, entitiesSecondType))
   })
 
+  /**
+    * apply a acceleration to the sentient to follow the target
+    * @param sentient sentient entity
+    * @param target target entity
+    */
   def followTarget(sentient: SentientProperty, target: SentientEnemyProperty): Unit = {
     val nextPositionTarget = target.getPositionComponent.point.add(target.getSpeedComponent.vector)
     val desiredVelocity = MathUtils.unitVector(nextPositionTarget, sentient.getPositionComponent.point) multiply MAX_SPEED
@@ -32,6 +37,12 @@ case class SentientSystem() extends AbstractSystemWithTwoTypeOfEntity[SentientPr
     applyAcceleration(sentient, steer)
   }
 
+  /**
+    *
+    * @param sentient sentient entity
+    * @param enemies list of entity
+    * @return the sentient's enemy with greater target coefficient is present, else None
+    */
   private def findTarget(sentient: SentientProperty, enemies: ListBuffer[SentientEnemyProperty]): Option[SentientEnemyProperty] =
     enemies.filter(e => !(e.getTypeComponent.typeEntity == EntityType.AntiMatter) &&
                         sentient.getDimensionComponent.radius > e.getDimensionComponent.radius &&
@@ -40,13 +51,31 @@ case class SentientSystem() extends AbstractSystemWithTwoTypeOfEntity[SentientPr
            .sortWith((a, b) => a._2 >  b._2 )
            .headOption map (_._1)
 
+  /**
+    *
+    * @param sentient sentient entity
+    * @param enemy sentient enemy entity
+    * @return a coefficient directly proportional to the enemy's radius and
+    *         inversely proportional to the distance between the entities
+    */
   private def targetCoefficient(sentient: SentientProperty, enemy: SentientEnemyProperty): Double =
     enemy.getDimensionComponent.radius / MathUtils.euclideanDistance(sentient.getPositionComponent, enemy.getPositionComponent)
 
+  /**
+    * search sentient enemies
+    * @param sentient sentient entity
+    * @param enemies list of all entities
+    * @return list of sentient's enemies
+    */
   private def findEnemies(sentient: SentientProperty, enemies: ListBuffer[SentientEnemyProperty]): List[SentientEnemyProperty] =
     enemies.filter(e => e.getTypeComponent.typeEntity == EntityType.AntiMatter ||
                   sentient.getDimensionComponent.radius < e.getDimensionComponent.radius) toList
 
+  /**
+    * applay acceleration to run away from all enemies
+    * @param sentient sentient entity
+    * @param enemies list of enemies
+    */
   private def runAwayFromEnemies(sentient: SentientProperty, enemies: List[SentientEnemyProperty]): Unit = {
     val desideredSeparation = sentient.getDimensionComponent.radius * COEFFICIENT_DESIDERED_SEPARATION
     val steer = average(enemies.map(e => (e, MathUtils.euclideanDistance(sentient.getPositionComponent, e.getPositionComponent)))
