7f5472279530a72a50c1876e694de0c928036a1f
Giacomo <giacomo.venturini2@studio.unibo.it>
2018-08-24 12:35:28 +0200

    remove useless code, add correctly acceleration assignment to sentient cells, improve test

--- src/main/scala/it/unibo/osmos/redux/ecs/systems/SentientPrologSystem.scala
+++ src/main/scala/it/unibo/osmos/redux/ecs/systems/SentientPrologSystem.scala
@@ -4,34 +4,30 @@ import alice.tuprolog._
 import it.unibo.osmos.redux.ecs.entities.{SentientEnemyProperty, SentientProperty}
 import it.unibo.osmos.redux.utils.PrologRules
 import it.unibo.osmos.redux.utils.Scala2P._
-
-import scala.collection.mutable.ListBuffer
+import it.unibo.osmos.redux.utils.Vector
 
 case class SentientPrologSystem() extends AbstractSystemWithTwoTypeOfEntity[SentientProperty, SentientEnemyProperty]() {
 
-  private val engine: Term => Stream[SolveInfo] = mkPrologEngine(PrologRules.rules)
+  private val prologEngine: Term => Stream[SolveInfo] = mkPrologEngine(PrologRules.rules)
 
   override protected def getGroupProperty: Class[SentientProperty] = classOf[SentientProperty]
 
   override protected def getGroupPropertySecondType: Class[SentientEnemyProperty] = classOf[SentientEnemyProperty]
 
   override def update(): Unit = {
-    //engine("followTarget(1,2,T)") foreach (println(_))
     entities foreach (sentientEntity => {
-      //println(sentientEntity)
-      computeSentientCellBehaviour(sentientEntity, entitiesSecondType)
+      val input = new Struct("sentientCellBehaviour", sentientEntity, sentientEnemiesToTerm(entitiesSecondType), "[RX,RY]")
+      prologEngine(input) headOption match {
+        case Some(value) =>
+          val sentientCellAccelerationX = value.getTerm("RX").toString.toDouble
+          val sentientCellAccelerationY = value.getTerm("RY").toString.toDouble
+          // println(sentientCellAccelerationX + " " + sentientCellAccelerationY)
+          val computedAcceleration = Vector(sentientCellAccelerationX, sentientCellAccelerationY)
+          val acceleration = sentientEntity.getAccelerationComponent
+          acceleration.vector_(acceleration.vector add computedAcceleration)
+        case _ =>
+      }
     })
-  }
-
-  private def computeSentientCellBehaviour(sentientEntity: SentientProperty, sentientEnemies: ListBuffer[SentientEnemyProperty]): Unit = {
-    //val sentientRadius: Double = sentientEntity.getDimensionComponent.radius
-    //println(sentientRadius)
-    val e = sentientEnemies.head
-    //val input = new Struct("followTarget", sentientEntity, e, new Var())
-    //val input = new Struct("add", "[1,2]", "[1,2]", new Var("A"))
-    //val input = new Struct("pow", new Int(3), new Int(3), new Var("A"))
-    val input = new Struct("followTarget", "[[10,30],[30,20]]","[[70,80],[40,30]]", new Var("A"))
-    engine(input) foreach (println(_))
   }
 
 }
