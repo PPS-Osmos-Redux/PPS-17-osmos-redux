37d926da7d6697c0f3ce7a2bb6a6f403a619f307
Giacomo <giacomo.venturini2@studio.unibo.it>
2018-08-27 11:11:30 +0200

    move tests from TestMovementSystem to TestCollisionSystem according to systems implementation change

--- src/test/scala/it/unibo/osmos/redux/TestCollisionSystem.scala
+++ src/test/scala/it/unibo/osmos/redux/TestCollisionSystem.scala
@@ -3,25 +3,42 @@ package it.unibo.osmos.redux
 import it.unibo.osmos.redux.ecs.components._
 import it.unibo.osmos.redux.ecs.entities.{CellEntity, EntityManager}
 import it.unibo.osmos.redux.ecs.systems.CollisionSystem
-import it.unibo.osmos.redux.mvc.model.MapShape.Rectangle
-import it.unibo.osmos.redux.mvc.model.{CollisionRules, Level, LevelMap, VictoryRules}
+import it.unibo.osmos.redux.mvc.model.MapShape.{Circle, Rectangle}
+import it.unibo.osmos.redux.mvc.model._
 import it.unibo.osmos.redux.utils.Point
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite}
 
-class TestCollisionSystem extends FunSuite {
+class TestCollisionSystem extends FunSuite with BeforeAndAfter {
 
-  val entity1 = CellEntity(AccelerationComponent(0,0), CollidableComponent(true), DimensionComponent(5),
+  val entity1 = CellEntity(AccelerationComponent(0, 0), CollidableComponent(true), DimensionComponent(5),
     PositionComponent(Point(20, 30)), SpeedComponent(0, 0), VisibleComponent(true), TypeComponent(EntityType.Matter))
-  val entity2 = CellEntity(AccelerationComponent(0,0), CollidableComponent(true), DimensionComponent(2),
+  val entity2 = CellEntity(AccelerationComponent(0, 0), CollidableComponent(true), DimensionComponent(2),
     PositionComponent(Point(60, 80)), SpeedComponent(0, 0), VisibleComponent(true), TypeComponent(EntityType.Matter))
-  val antiMatterEntity = CellEntity(AccelerationComponent(0,0), CollidableComponent(true), DimensionComponent(2),
+  val antiMatterEntity = CellEntity(AccelerationComponent(0, 0), CollidableComponent(true), DimensionComponent(2),
     PositionComponent(Point(65, 81)), SpeedComponent(0, 0), VisibleComponent(true), TypeComponent(EntityType.AntiMatter))
 
-  val levelInfo = Level(1,
+  var levelInfo: Level = _
+  /*Level(1,
     LevelMap( Rectangle((100, 100), 100, 150), CollisionRules.bouncing),
     null,
     VictoryRules.becomeTheBiggest,
-    false)
+    false)*/
+
+  before {
+    setupLevelInfo(Rectangle((100, 100), 100, 150), CollisionRules.bouncing)
+  }
+
+  after {
+    EntityManager.clear()
+  }
+
+  private def setupLevelInfo(mapShape: MapShape, collisionRules: CollisionRules.Value) {
+    levelInfo = Level(1,
+      LevelMap(mapShape, collisionRules),
+      null,
+      VictoryRules.becomeTheBiggest,
+      false)
+  }
 
   test("CollisionSystem should not collide the entity with herself") {
     val system = CollisionSystem(levelInfo)
@@ -97,9 +114,9 @@ class TestCollisionSystem extends FunSuite {
     system.update()
 
     assert(entity1.getDimensionComponent.radius == 5.2)
-    assert(entity1.getPositionComponent.point == Point(59.5,80))
+    assert(entity1.getPositionComponent.point == Point(59.5, 80))
     assert(entity2.getDimensionComponent.radius == 1.8)
-    assert(entity2.getPositionComponent.point == Point(66.5,80))
+    assert(entity2.getPositionComponent.point == Point(66.5, 80))
   }
 
   test("Collision with AntiMatter entity should reduce both dimension's entity") {
@@ -121,5 +138,178 @@ class TestCollisionSystem extends FunSuite {
 
     assert(entity1.getDimensionComponent.radius < originalDim1.radius && entity1.getAccelerationComponent != originalAccel1 &&
       antiMatterEntity.getDimensionComponent.radius < originalDim2.radius && antiMatterEntity.getAccelerationComponent != originalAccel2)
+  }
+
+  test("Collision with rectangular shape border, using bouncing collision rule, bounces entities back") {
+    setupLevelInfo(Rectangle((160, 100), 100, 160), CollisionRules.bouncing)
+    val system = CollisionSystem(levelInfo)
+
+    val lcca = AccelerationComponent(0, 0)
+    val lccc = CollidableComponent(true)
+    val lccd = DimensionComponent(2)
+    val lccp = PositionComponent(Point(79, 58))
+    val lccs = SpeedComponent(-4, 2)
+    val lccv = VisibleComponent(true)
+    val lcct = TypeComponent(EntityType.Matter)
+    val leftCollisionCellEntity = CellEntity(lcca, lccc, lccd, lccp, lccs, lccv, lcct)
+
+    val rcca = AccelerationComponent(0, 0)
+    val rccc = CollidableComponent(true)
+    val rccd = DimensionComponent(7)
+    val rccp = PositionComponent(Point(237, 90))
+    val rccs = SpeedComponent(6, 0)
+    val rccv = VisibleComponent(true)
+    val rcct = TypeComponent(EntityType.Matter)
+    val rightCollisionCellEntity = CellEntity(rcca, rccc, rccd, rccp, rccs, rccv, rcct)
+
+    val tcca = AccelerationComponent(0, 0)
+    val tccc = CollidableComponent(true)
+    val tccd = DimensionComponent(8)
+    val tccp = PositionComponent(Point(166, 56))
+    val tccs = SpeedComponent(6, -4)
+    val tccv = VisibleComponent(true)
+    val tcct = TypeComponent(EntityType.Matter)
+    val topCollisionCellEntity = CellEntity(tcca, tccc, tccd, tccp, tccs, tccv, tcct)
+
+    val bcca = AccelerationComponent(0, 0)
+    val bccc = CollidableComponent(true)
+    val bccd = DimensionComponent(5)
+    val bccp = PositionComponent(Point(113, 151))
+    val bccs = SpeedComponent(-2, 7)
+    val bccv = VisibleComponent(true)
+    val bcct = TypeComponent(EntityType.Matter)
+    val bottomCollisionCellEntity = CellEntity(bcca, bccc, bccd, bccp, bccs, bccv, bcct)
+
+    EntityManager.add(leftCollisionCellEntity)
+    EntityManager.add(rightCollisionCellEntity)
+    EntityManager.add(topCollisionCellEntity)
+    EntityManager.add(bottomCollisionCellEntity)
+
+    system.update()
+
+    assert(leftCollisionCellEntity.getSpeedComponent == SpeedComponent(4.0, 2.0))
+    assert(leftCollisionCellEntity.getPositionComponent.point == Point(85.0, 58.0))
+
+    assert(rightCollisionCellEntity.getSpeedComponent == SpeedComponent(-6.0, 0.0))
+    assert(rightCollisionCellEntity.getPositionComponent.point == Point(229.0, 90.0))
+
+    assert(topCollisionCellEntity.getSpeedComponent == SpeedComponent(6.0, 4.0))
+    assert(topCollisionCellEntity.getPositionComponent.point == Point(166.0, 60.0))
+
+    assert(bottomCollisionCellEntity.getSpeedComponent == SpeedComponent(-2.0, -7.0))
+    assert(bottomCollisionCellEntity.getPositionComponent.point == Point(113.0, 139.0))
+  }
+
+  test("Collision with rectangular shape border, using instant death collision rule, reduces entities' radius") {
+    setupLevelInfo(Rectangle((160, 100), 100, 160), CollisionRules.instantDeath)
+    val system = CollisionSystem(levelInfo)
+
+    val lcca = AccelerationComponent(0, 0)
+    val lccc = CollidableComponent(true)
+    val lccd = DimensionComponent(2)
+    val lccp = PositionComponent(Point(79, 58))
+    val lccs = SpeedComponent(-4, 2)
+    val lccv = VisibleComponent(true)
+    val lcct = TypeComponent(EntityType.Matter)
+    val leftCollisionCellEntity = CellEntity(lcca, lccc, lccd, lccp, lccs, lccv, lcct)
+
+    val rcca = AccelerationComponent(0, 0)
+    val rccc = CollidableComponent(true)
+    val rccd = DimensionComponent(7)
+    val rccp = PositionComponent(Point(237, 90))
+    val rccs = SpeedComponent(6, 0)
+    val rccv = VisibleComponent(true)
+    val rcct = TypeComponent(EntityType.Matter)
+    val rightCollisionCellEntity = CellEntity(rcca, rccc, rccd, rccp, rccs, rccv, rcct)
+
+    val tcca = AccelerationComponent(0, 0)
+    val tccc = CollidableComponent(true)
+    val tccd = DimensionComponent(8)
+    val tccp = PositionComponent(Point(166, 56))
+    val tccs = SpeedComponent(6, -4)
+    val tccv = VisibleComponent(true)
+    val tcct = TypeComponent(EntityType.Matter)
+    val topCollisionCellEntity = CellEntity(tcca, tccc, tccd, tccp, tccs, tccv, tcct)
+
+    val bcca = AccelerationComponent(0, 0)
+    val bccc = CollidableComponent(true)
+    val bccd = DimensionComponent(5)
+    val bccp = PositionComponent(Point(113, 151))
+    val bccs = SpeedComponent(-2, 7)
+    val bccv = VisibleComponent(true)
+    val bcct = TypeComponent(EntityType.Matter)
+    val bottomCollisionCellEntity = CellEntity(bcca, bccc, bccd, bccp, bccs, bccv, bcct)
+
+    EntityManager.add(leftCollisionCellEntity)
+    EntityManager.add(rightCollisionCellEntity)
+    EntityManager.add(topCollisionCellEntity)
+    EntityManager.add(bottomCollisionCellEntity)
+
+    system.update()
+
+    assert(leftCollisionCellEntity.getSpeedComponent == lccs)
+    assert(leftCollisionCellEntity.getPositionComponent.point == Point(79.0, 58.0))
+    assert(leftCollisionCellEntity.getDimensionComponent.radius == -1)
+
+    assert(rightCollisionCellEntity.getSpeedComponent == rccs)
+    assert(rightCollisionCellEntity.getPositionComponent.point == Point(237.0, 90.0))
+    assert(rightCollisionCellEntity.getDimensionComponent.radius == 3.0)
+
+    assert(topCollisionCellEntity.getSpeedComponent == tccs)
+    assert(topCollisionCellEntity.getPositionComponent.point == Point(166.0, 56.0))
+    assert(topCollisionCellEntity.getDimensionComponent.radius == 6.0)
+
+    assert(bottomCollisionCellEntity.getSpeedComponent == bccs)
+    assert(bottomCollisionCellEntity.getPositionComponent.point == Point(113.0, 151.0))
+    assert(bottomCollisionCellEntity.getDimensionComponent.radius == -1)
+  }
+
+  test("Collision with circular shape border, using bouncing collision rule, bounces entities back") {
+    val levelCenter = Point(300.0, 300.0)
+    val levelRadius = 200.0
+    val mapShape = Circle((levelCenter.x, levelCenter.y), levelRadius)
+    setupLevelInfo(mapShape, CollisionRules.bouncing)
+    val system = CollisionSystem(levelInfo)
+
+    val ca = AccelerationComponent(0, 0)
+    val cc = CollidableComponent(true)
+    val cd = DimensionComponent(20)
+    val cp = PositionComponent(Point(108, 280))
+    val cs = SpeedComponent(-10.0, -20.0)
+    val cv = VisibleComponent(true)
+    val ct = TypeComponent(EntityType.Matter)
+    val cellEntity = CellEntity(ca, cc, cd, cp, cs, cv, ct)
+
+    EntityManager.add(cellEntity)
+
+    system.update()
+
+    assert(cellEntity.getPositionComponent.point == Point(120.04654311426577, 304.09308622853155))
+    assert(cellEntity.getSpeedComponent.vector == utils.Vector(9.080318896799085, -20.43398660889337))
+  }
+
+  test("Collision with circular shape border, using instant death collision rule, reduces entities' radius") {
+    val levelCenter = Point(300.0, 300.0)
+    val levelRadius = 200.0
+    val mapShape = Circle((levelCenter.x, levelCenter.y), levelRadius)
+    setupLevelInfo(mapShape, CollisionRules.instantDeath)
+    val system = CollisionSystem(levelInfo)
+
+    val ca = AccelerationComponent(0, 0)
+    val cc = CollidableComponent(true)
+    val cd = DimensionComponent(20)
+    val cp = PositionComponent(Point(108, 280))
+    val cs = SpeedComponent(-10.0, -20.0)
+    val cv = VisibleComponent(true)
+    val ct = TypeComponent(EntityType.Matter)
+    val cellEntity = CellEntity(ca, cc, cd, cp, cs, cv, ct)
+
+    EntityManager.add(cellEntity)
+
+    system.update()
+
+    assert(cellEntity.getPositionComponent.point == Point(108.0, 280.0))
+    assert(cellEntity.getSpeedComponent.vector == utils.Vector(-10.0, -20.0))
+    assert(cellEntity.getDimensionComponent.radius == 6.961143807781525)
   }
 }
--- src/test/scala/it/unibo/osmos/redux/TestMovementSystem.scala
+++ src/test/scala/it/unibo/osmos/redux/TestMovementSystem.scala
@@ -3,32 +3,17 @@ package it.unibo.osmos.redux
 import it.unibo.osmos.redux.ecs.components._
 import it.unibo.osmos.redux.ecs.entities.{CellEntity, EntityManager, PlayerCellEntity}
 import it.unibo.osmos.redux.ecs.systems.MovementSystem
-import it.unibo.osmos.redux.mvc.model.MapShape.{Circle, Rectangle}
-import it.unibo.osmos.redux.mvc.model._
-import it.unibo.osmos.redux.utils.{MathUtils, Point}
+import it.unibo.osmos.redux.utils.Point
 import org.scalatest.{BeforeAndAfter, FunSuite}
 
 class TestMovementSystem extends FunSuite with BeforeAndAfter {
-
-  var movementSystem: MovementSystem = _
 
   after {
     EntityManager.clear()
   }
 
-  private def initEntityManager(mapShape: MapShape, collisionRules: CollisionRules.Value) {
-    val levelInfo = Level(1,
-      LevelMap(mapShape, collisionRules),
-      null,
-      VictoryRules.becomeTheBiggest,
-      false)
-    movementSystem = MovementSystem()
-    EntityManager.subscribe(movementSystem, null)
-  }
-
-  test("Speed and position update") {
-    val mapShape = Rectangle((100, 150), 100, 150)
-    initEntityManager(mapShape, CollisionRules.bouncing)
+  test("MovableProperty entities' acceleration, speed and position are updated correctly") {
+    val movementSystem = MovementSystem()
 
     val ca = AccelerationComponent(1, 1)
     val cc = CollidableComponent(true)
@@ -62,222 +47,52 @@ class TestMovementSystem extends FunSuite with BeforeAndAfter {
     assert(playerCellEntity.getPositionComponent.point == Point(130.0, 149.0))
     assert(playerCellEntity.getAccelerationComponent == AccelerationComponent(0.0, 0.0))
   }
+}
 
-  test("Rectangular shape field bouncing") {
-    val mapShape = Rectangle((160, 100), 100, 160)
-    initEntityManager(mapShape, CollisionRules.bouncing)
+/*
+private def computePositionAfterBounce(currentPosition: Point, precPosition: Point, levelRadius: Double, levelCenter: Point): Point = {
+val straightLine = GeometricalStraightLine(currentPosition, precPosition)
+val circumference = GeometricalCircumference(levelCenter, levelRadius)
 
-    val lcca = AccelerationComponent(0, 0)
-    val lccc = CollidableComponent(true)
-    val lccd = DimensionComponent(2)
-    val lccp = PositionComponent(Point(83, 56))
-    val lccs = SpeedComponent(-4, 2)
-    val lccv = VisibleComponent(true)
-    val lcct = TypeComponent(EntityType.Matter)
-    val leftCollisionCellEntity = CellEntity(lcca, lccc, lccd, lccp, lccs, lccv, lcct)
+// computing intersection between straight line and circumference
+val eq_a = 1 + Math.pow(straightLine.m, 2)
+val eq_b = 2 * straightLine.m * straightLine.q + circumference.a + circumference.b * straightLine.m
+val eq_c = Math.pow(straightLine.q, 2) + circumference.b * straightLine.q + circumference.c
+val delta = Math.pow(eq_b, 2) - 4 * eq_a * eq_c
 
-    val rcca = AccelerationComponent(0, 0)
-    val rccc = CollidableComponent(true)
-    val rccd = DimensionComponent(7)
-    val rccp = PositionComponent(Point(231, 90))
-    val rccs = SpeedComponent(6, 0)
-    val rccv = VisibleComponent(true)
-    val rcct = TypeComponent(EntityType.Matter)
-    val rightCollisionCellEntity = CellEntity(rcca, rccc, rccd, rccp, rccs, rccv, rcct)
+// x1,x2 = (-b ± sqrt(Δ) / 2 * a)
+val x_1 = (-eq_b + Math.sqrt(delta)) / 2 * eq_a
+val y_1 = straightLine.m * x_1 + straightLine.q
+val p_1 = Point(x_1, y_1)
 
-    val tcca = AccelerationComponent(0, 0)
-    val tccc = CollidableComponent(true)
-    val tccd = DimensionComponent(8)
-    val tccp = PositionComponent(Point(160, 60))
-    val tccs = SpeedComponent(6, -4)
-    val tccv = VisibleComponent(true)
-    val tcct = TypeComponent(EntityType.Matter)
-    val topCollisionCellEntity = CellEntity(tcca, tccc, tccd, tccp, tccs, tccv, tcct)
+var tangentToCircumference: GeometricalStraightLine = null
 
-    val bcca = AccelerationComponent(0, 0)
-    val bccc = CollidableComponent(true)
-    val bccd = DimensionComponent(5)
-    val bccp = PositionComponent(Point(115, 144))
-    val bccs = SpeedComponent(-2, 7)
-    val bccv = VisibleComponent(true)
-    val bcct = TypeComponent(EntityType.Matter)
-    val bottomCollisionCellEntity = CellEntity(bcca, bccc, bccd, bccp, bccs, bccv, bcct)
+MathUtils.isPointBetweenPoints(p_1, precPosition, currentPosition) match {
+case true =>
+val straightLineFromCenterToP_1 = GeometricalStraightLine(p_1, levelCenter)
+val tang_m = -1 / straightLineFromCenterToP_1.m
+val tang_q = straightLineFromCenterToP_1.q
+tangentToCircumference = GeometricalStraightLine(tang_m, tang_q)
+case false =>
+// must compute second result
+val x_2 = (-eq_b - Math.sqrt(delta)) / 2 * eq_a
+val y_2 = straightLine.m * x_2 + straightLine.q
+val p_2 = Point(x_2, y_2)
 
-    EntityManager.add(leftCollisionCellEntity)
-    EntityManager.add(rightCollisionCellEntity)
-    EntityManager.add(topCollisionCellEntity)
-    EntityManager.add(bottomCollisionCellEntity)
+val straightLineFromCenterToP_2 = GeometricalStraightLine(p_2, levelCenter)
+val tang_m = -1 / straightLineFromCenterToP_2.m
+val tang_q = straightLineFromCenterToP_2.q
+tangentToCircumference = GeometricalStraightLine(tang_m, tang_q)
+}
 
-    movementSystem.update()
+val perpendicular_m = -1 / tangentToCircumference.m
+val perpendicular_q = 1 / tangentToCircumference.m * currentPosition.x + currentPosition.y
+val perpendicularOfTangent = GeometricalStraightLine(perpendicular_m, perpendicular_q)
 
-    assert(leftCollisionCellEntity.getSpeedComponent == SpeedComponent(4.0, 2.0))
-    assert(leftCollisionCellEntity.getPositionComponent.point == Point(85.0, 58.0))
+val midPointX = (perpendicularOfTangent.q - tangentToCircumference.q) / (perpendicularOfTangent.m - tangentToCircumference.m)
+val midPointY = tangentToCircumference.m * midPointX + tangentToCircumference.q
 
-    assert(rightCollisionCellEntity.getSpeedComponent == SpeedComponent(-6.0, 0.0))
-    assert(rightCollisionCellEntity.getPositionComponent.point == Point(229.0, 90.0))
-
-    assert(topCollisionCellEntity.getSpeedComponent == SpeedComponent(6.0, 4.0))
-    assert(topCollisionCellEntity.getPositionComponent.point == Point(166.0, 60.0))
-
-    assert(bottomCollisionCellEntity.getSpeedComponent == SpeedComponent(-2.0, -7.0))
-    assert(bottomCollisionCellEntity.getPositionComponent.point == Point(113.0, 139.0))
-  }
-
-  test("Rectangular shape field instant death rule") {
-    val mapShape = Rectangle((160, 100), 100, 160)
-    initEntityManager(mapShape, CollisionRules.instantDeath)
-
-    val lcca = AccelerationComponent(0, 0)
-    val lccc = CollidableComponent(true)
-    val lccd = DimensionComponent(2)
-    val lccp = PositionComponent(Point(83, 56))
-    val lccs = SpeedComponent(-4, 2)
-    val lccv = VisibleComponent(true)
-    val lcct = TypeComponent(EntityType.Matter)
-    val leftCollisionCellEntity = CellEntity(lcca, lccc, lccd, lccp, lccs, lccv, lcct)
-
-    val rcca = AccelerationComponent(0, 0)
-    val rccc = CollidableComponent(true)
-    val rccd = DimensionComponent(7)
-    val rccp = PositionComponent(Point(231, 90))
-    val rccs = SpeedComponent(6, 0)
-    val rccv = VisibleComponent(true)
-    val rcct = TypeComponent(EntityType.Matter)
-    val rightCollisionCellEntity = CellEntity(rcca, rccc, rccd, rccp, rccs, rccv, rcct)
-
-    val tcca = AccelerationComponent(0, 0)
-    val tccc = CollidableComponent(true)
-    val tccd = DimensionComponent(8)
-    val tccp = PositionComponent(Point(160, 60))
-    val tccs = SpeedComponent(6, -4)
-    val tccv = VisibleComponent(true)
-    val tcct = TypeComponent(EntityType.Matter)
-    val topCollisionCellEntity = CellEntity(tcca, tccc, tccd, tccp, tccs, tccv, tcct)
-
-    val bcca = AccelerationComponent(0, 0)
-    val bccc = CollidableComponent(true)
-    val bccd = DimensionComponent(5)
-    val bccp = PositionComponent(Point(115, 144))
-    val bccs = SpeedComponent(-2, 7)
-    val bccv = VisibleComponent(true)
-    val bcct = TypeComponent(EntityType.Matter)
-    val bottomCollisionCellEntity = CellEntity(bcca, bccc, bccd, bccp, bccs, bccv, bcct)
-
-    EntityManager.add(leftCollisionCellEntity)
-    EntityManager.add(rightCollisionCellEntity)
-    EntityManager.add(topCollisionCellEntity)
-    EntityManager.add(bottomCollisionCellEntity)
-
-    movementSystem.update()
-
-    assert(leftCollisionCellEntity.getSpeedComponent == lccs)
-    assert(leftCollisionCellEntity.getPositionComponent.point == Point(79.0, 58.0))
-    assert(leftCollisionCellEntity.getDimensionComponent.radius == -1)
-
-    assert(rightCollisionCellEntity.getSpeedComponent == rccs)
-    assert(rightCollisionCellEntity.getPositionComponent.point == Point(237.0, 90.0))
-    assert(rightCollisionCellEntity.getDimensionComponent.radius == 3.0)
-
-    assert(topCollisionCellEntity.getSpeedComponent == tccs)
-    assert(topCollisionCellEntity.getPositionComponent.point == Point(166.0, 56.0))
-    assert(topCollisionCellEntity.getDimensionComponent.radius == 6.0)
-
-    assert(bottomCollisionCellEntity.getSpeedComponent == bccs)
-    assert(bottomCollisionCellEntity.getPositionComponent.point == Point(113.0, 151.0))
-    assert(bottomCollisionCellEntity.getDimensionComponent.radius == -1)
-  }
-
-  test("Circular shape field bouncing") {
-    val levelCenter = Point(300.0, 300.0)
-    val levelRadius = 200.0
-    val mapShape = Circle((levelCenter.x, levelCenter.y), levelRadius)
-    initEntityManager(mapShape, CollisionRules.bouncing)
-
-    val ca = AccelerationComponent(0, 0)
-    val cc = CollidableComponent(true)
-    val cd = DimensionComponent(20)
-    val cp = PositionComponent(Point(118, 300))
-    val cs = SpeedComponent(-10.0, -20.0)
-    val cv = VisibleComponent(true)
-    val ct = TypeComponent(EntityType.Matter)
-    val cellEntity = CellEntity(ca, cc, cd, cp, cs, cv, ct)
-
-    EntityManager.add(cellEntity)
-
-    movementSystem.update()
-
-    assert(cellEntity.getPositionComponent.point == Point(120.04654311426577, 304.09308622853155))
-    assert(cellEntity.getSpeedComponent.vector == utils.Vector(9.080318896799085, -20.43398660889337))
-  }
-
-  test("Circular shape field instant death") {
-    val levelCenter = Point(300.0, 300.0)
-    val levelRadius = 200.0
-    val mapShape = Circle((levelCenter.x, levelCenter.y), levelRadius)
-    initEntityManager(mapShape, CollisionRules.instantDeath)
-
-    val ca = AccelerationComponent(0, 0)
-    val cc = CollidableComponent(true)
-    val cd = DimensionComponent(20)
-    val cp = PositionComponent(Point(118, 300))
-    val cs = SpeedComponent(-10.0, -20.0)
-    val cv = VisibleComponent(true)
-    val ct = TypeComponent(EntityType.Matter)
-    val cellEntity = CellEntity(ca, cc, cd, cp, cs, cv, ct)
-
-    EntityManager.add(cellEntity)
-
-    movementSystem.update()
-
-    assert(cellEntity.getPositionComponent.point == Point(108.0, 280.0))
-    assert(cellEntity.getSpeedComponent.vector == utils.Vector(-10.0, -20.0))
-    assert(cellEntity.getDimensionComponent.radius == 6.961143807781525)
-  }
-
-  private def computePositionAfterBounce(currentPosition: Point, precPosition: Point, levelRadius: Double, levelCenter: Point): Point = {
-    val straightLine = GeometricalStraightLine(currentPosition, precPosition)
-    val circumference = GeometricalCircumference(levelCenter, levelRadius)
-
-    // computing intersection between straight line and circumference
-    val eq_a = 1 + Math.pow(straightLine.m, 2)
-    val eq_b = 2 * straightLine.m * straightLine.q + circumference.a + circumference.b * straightLine.m
-    val eq_c = Math.pow(straightLine.q, 2) + circumference.b * straightLine.q + circumference.c
-    val delta = Math.pow(eq_b, 2) - 4 * eq_a * eq_c
-
-    // x1,x2 = (-b ± sqrt(Δ) / 2 * a)
-    val x_1 = (-eq_b + Math.sqrt(delta)) / 2 * eq_a
-    val y_1 = straightLine.m * x_1 + straightLine.q
-    val p_1 = Point(x_1, y_1)
-
-    var tangentToCircumference: GeometricalStraightLine = null
-
-    MathUtils.isPointBetweenPoints(p_1, precPosition, currentPosition) match {
-      case true =>
-        val straightLineFromCenterToP_1 = GeometricalStraightLine(p_1, levelCenter)
-        val tang_m = -1 / straightLineFromCenterToP_1.m
-        val tang_q = straightLineFromCenterToP_1.q
-        tangentToCircumference = GeometricalStraightLine(tang_m, tang_q)
-      case false =>
-        // must compute second result
-        val x_2 = (-eq_b - Math.sqrt(delta)) / 2 * eq_a
-        val y_2 = straightLine.m * x_2 + straightLine.q
-        val p_2 = Point(x_2, y_2)
-
-        val straightLineFromCenterToP_2 = GeometricalStraightLine(p_2, levelCenter)
-        val tang_m = -1 / straightLineFromCenterToP_2.m
-        val tang_q = straightLineFromCenterToP_2.q
-        tangentToCircumference = GeometricalStraightLine(tang_m, tang_q)
-    }
-
-    val perpendicular_m = -1 / tangentToCircumference.m
-    val perpendicular_q = 1 / tangentToCircumference.m * currentPosition.x + currentPosition.y
-    val perpendicularOfTangent = GeometricalStraightLine(perpendicular_m, perpendicular_q)
-
-    val midPointX = (perpendicularOfTangent.q - tangentToCircumference.q) / (perpendicularOfTangent.m - tangentToCircumference.m)
-    val midPointY = tangentToCircumference.m * midPointX + tangentToCircumference.q
-
-    Point(2 * midPointX - currentPosition.x, 2 * midPointY - currentPosition.y)
-  }
+Point(2 * midPointX - currentPosition.x, 2 * midPointY - currentPosition.y)
 }
 
 trait GeometricalStraightLine {
@@ -324,3 +139,4 @@ object GeometricalCircumference {
   }
 
 }
+*/
