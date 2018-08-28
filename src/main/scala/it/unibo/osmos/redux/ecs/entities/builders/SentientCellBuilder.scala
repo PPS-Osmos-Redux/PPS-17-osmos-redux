015802bb90bf95e9de16100143c88341ef9d576e
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-28 19:38:07 +0200

    fix throw exception

--- src/main/scala/it/unibo/osmos/redux/ecs/entities/builders/SentientCellBuilder.scala
+++ src/main/scala/it/unibo/osmos/redux/ecs/entities/builders/SentientCellBuilder.scala
@@ -1,10 +1,10 @@
 package it.unibo.osmos.redux.ecs.entities.builders
-import it.unibo.osmos.redux.ecs.components.EntityType
-import it.unibo.osmos.redux.ecs.entities.SentientCellEntity
+import it.unibo.osmos.redux.ecs.entities.{EntityType, SentientCellEntity}
 
 case class SentientCellBuilder() extends CellBuilder {
 
-  override def withEntityType(entityType: EntityType.Value): CellBuilder = throw UnsupportedOperationException
+  override def withEntityType(entityType: EntityType.Value): CellBuilder =
+    throw new UnsupportedOperationException("Is not possible set entity type for SentientCellEntity")
 
   override def build: SentientCellEntity = {
     checkMultipleBuild()
