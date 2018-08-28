f843186973b44eff8a80f8e527a8ac82fd0b19ea
Placu <andrea.placuzzi@studio.unibo.it>
2018-08-28 19:45:36 +0200

    fix rename method name

--- src/main/scala/it/unibo/osmos/redux/ecs/systems/DrawSystem.scala
+++ src/main/scala/it/unibo/osmos/redux/ecs/systems/DrawSystem.scala
@@ -16,10 +16,10 @@ case class DrawSystem(entitiesDrawer: EntitiesDrawer) extends AbstractSystemWith
   override def update(): Unit = entitiesDrawer.drawEntities(getPlayerEntity, getEntities)
 
   private def getPlayerEntity: Option[DrawableWrapper] =
-    entitiesSecondType find (p => p.getVisibleComponent.isVisible())  map(p => drawablePropertyToDrawableWrapper(p))
+    entitiesSecondType find (p => p.getVisibleComponent.isVisible)  map(p => drawablePropertyToDrawableWrapper(p))
 
   private def getEntities: List[DrawableWrapper] =
-    entities filter(e => e.getVisibleComponent.isVisible()) map(e => drawablePropertyToDrawableWrapper(e)) toList
+    entities filter(e => e.getVisibleComponent.isVisible) map(e => drawablePropertyToDrawableWrapper(e)) toList
 
   private def drawablePropertyToDrawableWrapper(entity: DrawableProperty): DrawableWrapper =
     DrawableWrapper(entity.getPositionComponent.point,
