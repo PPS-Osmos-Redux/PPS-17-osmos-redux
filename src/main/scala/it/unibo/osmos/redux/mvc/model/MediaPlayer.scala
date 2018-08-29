2d16f1be0631fcc114dce4a40c6636db60c809f9
Andrea Procucci <Andrea Procucci>
2018-08-29 09:06:22 +0200

    Add button sound play

--- src/main/scala/it/unibo/osmos/redux/mvc/model/MediaPlayer.scala
+++ src/main/scala/it/unibo/osmos/redux/mvc/model/MediaPlayer.scala
@@ -4,13 +4,17 @@ import it.unibo.osmos.redux.mvc.controller.FileManager
 import javafx.scene.media.MediaPlayer.Status
 import javafx.scene.media.{Media, MediaPlayer}
 import javafx.util
+import scalafx.scene.media.AudioClip
+
 object SoundsType extends Enumeration {
-  val menu, level = Value
+  val menu, level, button = Value
 }
 
 object MediaPlayer {
   private var mediaPlayer:Option[MediaPlayer] = None
   private var lastLoadedSound:Option[String] = None
+  private var buttonAudioClip:Option[AudioClip] = None
+  private var volume:Double = 100
 
   private def canApplyStateChange(allowedStates:List[Status]):Boolean = mediaPlayer match {
     case Some(mp) if allowedStates.contains(mp.getStatus) => true
@@ -36,10 +40,16 @@ object MediaPlayer {
   def play(sound:SoundsType.Value): Unit = sound match {
     case SoundsType.menu => checkMediaPlayerStatus(FileManager.loadMenuMusic())
     case SoundsType.level => checkMediaPlayerStatus(FileManager.loadLevelMusic())
+    case SoundsType.button => playButtonSound(FileManager.loadButtonsSound())
     case _ => println("Sound type not managed!");
   }
 
   def stop(): Unit = if(canApplyStateChange(List(Status.PLAYING, Status.PAUSED))) mediaPlayer = None
 
   def pause(): Unit =  if(canApplyStateChange(List(Status.PLAYING))) mediaPlayer.get.pause()
+
+  private def playButtonSound(sound:String): Unit = buttonAudioClip match {
+    case Some(bac) => bac.play(volume)
+    case _ => buttonAudioClip = Some(new AudioClip(FileManager.loadButtonsSound())); playButtonSound(sound)
+  }
 }
