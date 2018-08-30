1b13de3844bda02ea433fe37f21382764cb207d9
DavideGia <davide.giacomini2@studio.unibo.it>
2018-08-30 12:46:07 +0200

    Fixed: MultiPlayerUpdateSystem was broadcasting the wrong message and entities collection

--- src/main/scala/it/unibo/osmos/redux/ecs/systems/MultiPlayerUpdateSystem.scala
+++ src/main/scala/it/unibo/osmos/redux/ecs/systems/MultiPlayerUpdateSystem.scala
@@ -2,18 +2,19 @@ package it.unibo.osmos.redux.ecs.systems
 
 import it.unibo.osmos.redux.ecs.entities.DrawableProperty
 import it.unibo.osmos.redux.multiplayer.server.Server
-import it.unibo.osmos.redux.mvc.view.drawables.DrawableWrapper
+import it.unibo.osmos.redux.multiplayer.server.ServerActor.UpdateGame
+import it.unibo.osmos.redux.mvc.view.drawables.DrawableEntity
 
 case class MultiPlayerUpdateSystem(server: Server) extends AbstractSystem[DrawableProperty] {
 
   override protected def getGroupProperty: Class[DrawableProperty] = classOf[DrawableProperty]
 
-  override def update(): Unit = server.broadcastMessage(getEntities)
+  override def update(): Unit = server.broadcastMessage(UpdateGame(getEntities))
 
-  private def getEntities: Seq[DrawableWrapper] =
+  private def getEntities: Seq[DrawableEntity] =
     entities filter(_.getVisibleComponent.isVisible()) map wrapToDrawable
 
-  private def wrapToDrawable(entity: DrawableProperty): DrawableWrapper =
-    DrawableWrapper(entity.getPositionComponent.point, entity.getDimensionComponent.radius,
+  private def wrapToDrawable(entity: DrawableProperty): DrawableEntity =
+    new DrawableEntity(entity.getUUID, entity.getPositionComponent.point, entity.getDimensionComponent.radius,
       (entity.getSpeedComponent.vector.x, entity.getSpeedComponent.vector.y), entity.getTypeComponent.typeEntity)
 }
