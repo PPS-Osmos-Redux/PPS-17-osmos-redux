a3b4cb7ef208fab19a83957dcdc7b2b7a3a00c3b
Marco Galassi <marco.galassi6@studio.unibo.it>
2018-09-02 09:39:46 +0200

    Add GenericResponse to hold messages and result of several functions

--- src/main/scala/it/unibo/osmos/redux/mvc/view/components/editor/RectangleLevelCreator.scala
+++ src/main/scala/it/unibo/osmos/redux/mvc/view/components/editor/RectangleLevelCreator.scala
@@ -2,9 +2,9 @@ package it.unibo.osmos.redux.mvc.view.components.editor
 
 import it.unibo.osmos.redux.mvc.model.MapShape
 import it.unibo.osmos.redux.mvc.view.components.custom.TitledDoubleField
-import scalafx.beans.property.{DoubleProperty, ObjectProperty}
-import scalafx.scene.control.{Label, TextField}
-import scalafx.scene.layout.{HBox, VBox}
+import scalafx.beans.property.DoubleProperty
+import scalafx.scene.control.Label
+import scalafx.scene.layout.VBox
 
 /**
   * A panel showing input nodes which is also capable of providing the requested rectangular level
