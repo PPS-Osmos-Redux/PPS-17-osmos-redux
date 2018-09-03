d4826d4bc38aaaf01b28e4341bd07e4576a01d8c
Placu <andrea.placuzzi@studio.unibo.it>
2018-09-03 13:00:40 +0200

    pimp list with method shiftDistance

--- src/main/scala/it/unibo/osmos/redux/ecs/systems/sentient/EscapeFromEnemiesRule.scala
+++ src/main/scala/it/unibo/osmos/redux/ecs/systems/sentient/EscapeFromEnemiesRule.scala
@@ -31,29 +31,15 @@ case class EscapeFromEnemiesRule(enemies: ListBuffer[SentientEnemyProperty]) ext
   private def escapeFromEnemies(sentient: SentientProperty, enemies: List[SentientEnemyProperty], previousAcceleration: Vector): Vector = {
     val actualSpeed = sentient.getSpeedComponent.vector add previousAcceleration
     val desiredSeparation = getDesiredSeparation(actualSpeed)
-    val filteredEnemies = enemies map(e => (e, computeDistance(sentient, e))) filter(p => p._2 < desiredSeparation)
-    shiftDistance(filteredEnemies)
+    enemies.map(e => (e, computeDistance(sentient, e)))
+      .filter(p => p._2 < desiredSeparation)
+      .shiftDistance(MIN_VALUE)
       .map(m => MathUtils.unitVector(sentient.getPositionComponent.point, m._1.getPositionComponent.point) divide m._2)
       .foldLeft((Vector.zero(), 1)) ((acc, i) => (acc._1 add ((i subtract acc._1) divide acc._2), acc._2 + 1))._1 normalized() match {
-      case unitVectorDesiredVelocity if unitVectorDesiredVelocity == Vector(0,0) => Vector.zero()
-      case unitVectorDesiredVelocity =>
-        computeSteer(actualSpeed, unitVectorDesiredVelocity) multiply WEIGHT_OF_ESCAPE_ACCELERATION_FROM_ENEMIES
-    }
-  }
-
-  /**
-    * if smallest distance(second value of tuple) is less or equal of 0,
-    * shift all distance of minus smallest distance plus Double.MinPositiveValue, so
-    * the smallest distance is equal to Double.MinPositiveValue
-    * @param list list to shift
-    * @return shifted list
-    */
-  private def shiftDistance(list: List[(SentientEnemyProperty, Double)]): List[(SentientEnemyProperty, Double)] = list match {
-    case Nil => Nil
-    case _ => list.min(Ordering.by((d:(SentientEnemyProperty, Double)) => d._2)) match {
-      case min if min._2 <= 1 => list.map(e => (e._1, e._2 - min._2 + MIN_VALUE))
-      case _ => list
-    }
+        case unitVectorDesiredVelocity if unitVectorDesiredVelocity == Vector(0,0) => Vector.zero()
+        case unitVectorDesiredVelocity =>
+          computeSteer(actualSpeed, unitVectorDesiredVelocity) multiply WEIGHT_OF_ESCAPE_ACCELERATION_FROM_ENEMIES
+      }
   }
 
   private def computeDistance(sentient: SentientProperty, enemy: SentientEnemyProperty): Double = {
--- src/main/scala/it/unibo/osmos/redux/ecs/systems/sentient/SentientUtils.scala
+++ src/main/scala/it/unibo/osmos/redux/ecs/systems/sentient/SentientUtils.scala
@@ -17,4 +17,23 @@ object SentientUtils {
 
   def hasLostRadiusBehaviour(sentient: SentientProperty): Boolean =
     sentient.getDimensionComponent.radius >= MIN_RADIUS_FOR_LOST_RADIUS_BEHAVIOUR
+
+  implicit class PimpedList[A](list: List[(A, Double)]) {
+
+    /**
+      * Find the smallest distance(second parameter of tuple value) inside the list,
+      * if it is less or equal than threshold, then
+      * shift all distance of minus smallest distance plus threshold, so that
+      * the smallest distance is equal to threshold
+      * @param threshold the threshold
+      * @return shifted list
+      */
+    def shiftDistance(threshold: Double): List[(A, Double)] = list match {
+      case Nil => Nil
+      case _ => list.min(Ordering.by((d:(A, Double)) => d._2)) match {
+        case min if min._2 <= threshold => list.map(e => (e._1, e._2 - min._2 + threshold))
+        case _ => list
+      }
+    }
+  }
 }
