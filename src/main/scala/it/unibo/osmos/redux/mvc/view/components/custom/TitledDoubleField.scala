cf44a2bf9af8305f98ed625e0f83dfb348a5708c
Marco Galassi <marco.galassi6@studio.unibo.it>
2018-09-06 23:47:08 +0200

    Update TitleDoubleField with proper double checking

--- src/main/scala/it/unibo/osmos/redux/mvc/view/components/custom/TitledDoubleField.scala
+++ src/main/scala/it/unibo/osmos/redux/mvc/view/components/custom/TitledDoubleField.scala
@@ -17,9 +17,22 @@ import scalafx.util.converter.{DoubleStringConverter, NumberStringConverter}
 class TitledDoubleField(override val title: StringProperty, private val value: DoubleProperty, private val minValue: Double = Double.MinValue, private val maxValue: Double = Double.MaxValue)
   extends TitledNode[TextField](title, vertical = false) {
 
+  /**
+    * Additional constructor
+    * @param title the title
+    * @param value the value, as a DoubleProperty
+    */
   def this(title: String, value: DoubleProperty) {
     this(StringProperty(title), value)
   }
+
+  /**
+    * Additional constructor
+    * @param title the title
+    * @param value the value, as a DoubleProperty
+    * @param minValue the minimum allowed value
+    * @param maxValue the maximum allowed value
+    */
   def this(title: String, value: DoubleProperty, minValue: Double, maxValue: Double) {
     this(StringProperty(title), value, minValue, maxValue)
   }
@@ -35,10 +48,19 @@ class TitledDoubleField(override val title: StringProperty, private val value: D
     prefWidth <== maxWidth
     textFormatter = new TextFormatter[Double](new DoubleStringConverter(), 0, { c: Change => {
       val input = c.controlNewText
-      val isNumber = input.matches("^[0-9]+(\\.[0-9]+)?$")
-      if (!isNumber) c.setText("")
-      if (isNumber && (maxValue <= c.getControlNewText.toDouble || minValue >= c.getControlNewText.toDouble)) c.setText("")
-      c
+      /** Checking if the double starts with the dot */
+      val dotted = input.matches("^(-?)(\\.[0-9]+)?$")
+      if (dotted) {
+        /** Adding 0 in front*/
+        c.setText("0")
+        c
+      } else {
+        /** Checking if its a double of any type */
+        val isNumber = input.matches("^(-?)(0|([1-9][0-9]*))(\\.[0-9]+)?$")
+        if (!isNumber) c.setText("")
+        if (isNumber && (maxValue <= c.getControlNewText.toDouble || minValue >= c.getControlNewText.toDouble)) c.setText("")
+        c
+      }
     }})
   }
 }
