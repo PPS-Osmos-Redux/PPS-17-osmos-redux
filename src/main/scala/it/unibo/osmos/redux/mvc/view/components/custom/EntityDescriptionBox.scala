bec16d1cc3857cb795eb0898c262f3592f303daf
Marco Galassi <marco.galassi6@studio.unibo.it>
2018-09-09 22:03:02 +0200

    Add basic EntityDescriptionBox

---
+++ src/main/scala/it/unibo/osmos/redux/mvc/view/components/custom/EntityDescriptionBox.scala
@@ -0,0 +1,22 @@
+package it.unibo.osmos.redux.mvc.view.components.custom
+
+import scalafx.scene.image.{Image, ImageView}
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color
+import scalafx.scene.text.Text
+
+/** View holding a entity image and its description in the game
+  *
+  * @param image the entity image
+  * @param description the entity description
+  */
+class EntityDescriptionBox(val image: Image, val description: String) extends HBox(10.0) {
+
+  private val imageView = new ImageView(image)
+
+  private val text = new Text(description) {
+    fill = Color.White
+  }
+
+  children = Seq(imageView, text)
+}
