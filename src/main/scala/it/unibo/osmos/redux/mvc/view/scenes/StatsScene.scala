33f5d0f0824385d3c3dc574a572def0df60f754a
Giacomo <giacomo.venturini2@studio.unibo.it>
2018-09-09 15:39:51 +0200

    implement table holding player stats

--- src/main/scala/it/unibo/osmos/redux/mvc/view/scenes/StatsScene.scala
+++ src/main/scala/it/unibo/osmos/redux/mvc/view/scenes/StatsScene.scala
@@ -2,21 +2,50 @@ package it.unibo.osmos.redux.mvc.view.scenes
 
 import it.unibo.osmos.redux.mvc.controller.levels.structure.CampaignLevel
 import it.unibo.osmos.redux.mvc.view.ViewConstants
+import javafx.collections.FXCollections
+import scalafx.beans.property.StringProperty
 import scalafx.geometry.Pos
-import scalafx.scene.control.TableView
+import scalafx.scene.control.{TableColumn, TableView}
 import scalafx.scene.layout.VBox
 import scalafx.stage.Stage
 
 class StatsScene(override val parentStage: Stage, listener: StatsSceneListener, previousSceneListener: BackClickListener) extends DefaultBackScene(parentStage, previousSceneListener) {
 
-  private val statsTable = new TableView[String]() {
+  private implicit def toStringProperty[A](value: A): StringProperty = StringProperty(value.toString)
+
+  private val levelNameColumn = new TableColumn[CampaignLevel, String]("Level Name") {
+    cellValueFactory = p => p.value.levelInfo.name
+  }
+  private val victoryRuleColumn = new TableColumn[CampaignLevel, String]("Victory Rule") {
+    cellValueFactory = p => p.value.levelInfo.victoryRule
+  }
+  private val victoriesColumn = new TableColumn[CampaignLevel, String]("Victories") {
+    cellValueFactory = p => p.value.levelStat.victories
+  }
+  private val defeatsColumn = new TableColumn[CampaignLevel, String]("Defeats") {
+    cellValueFactory = p => p.value.levelStat.defeats
+  }
+  
+  private val playerData = FXCollections.observableArrayList[CampaignLevel]()
+  listener.getCampaignLevels.foreach(e => playerData.add(e))
+
+  private val statsTable = new TableView[CampaignLevel]() {
     maxWidth = ViewConstants.Window.halfWindowWidth
     prefHeight = ViewConstants.Window.defaultWindowHeight / 4
+    /* add the columns to table*/
+    columns ++= List(levelNameColumn, victoryRuleColumn, victoriesColumn, defeatsColumn)
   }
 
-  /**
-    * The central container
-    */
+  private val columnMinWidth = statsTable.width / 4.1
+  levelNameColumn.minWidth <== columnMinWidth
+  victoryRuleColumn.minWidth <== columnMinWidth
+  victoriesColumn.minWidth <== columnMinWidth
+  defeatsColumn.minWidth <== columnMinWidth
+
+  /* add player data to table */
+  statsTable.setItems(playerData)
+
+  /** The central container */
   protected val container: VBox = new VBox(15) {
     alignment = Pos.Center
     children = Seq(statsTable, goBack)
