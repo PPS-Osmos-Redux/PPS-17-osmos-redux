5513e11de45184db0f19ad29690e789cdc960e87
Giacomo <giacomo.venturini2@studio.unibo.it>
2018-09-24 18:18:26 +0200

    refactor end game system to respect ISP

--- src/main/scala/it/unibo/osmos/redux/ecs/systems/EndGameSystem.scala
+++ src/main/scala/it/unibo/osmos/redux/ecs/systems/EndGameSystem.scala
@@ -1,7 +1,7 @@
 package it.unibo.osmos.redux.ecs.systems
 
+import it.unibo.osmos.redux.ecs.entities.EntityType
 import it.unibo.osmos.redux.ecs.entities.properties.composed.DeathProperty
-import it.unibo.osmos.redux.ecs.entities.{EntityType, PlayerCellEntity}
 import it.unibo.osmos.redux.ecs.systems.victoryconditions.{AbsorbCellsWithTypeVictoryCondition, BecomeHugeVictoryCondition, BecomeTheBiggestVictoryCondition}
 import it.unibo.osmos.redux.mvc.controller.levels.structure.VictoryRules
 import it.unibo.osmos.redux.mvc.view.context.GameStateHolder
@@ -12,7 +12,7 @@ import it.unibo.osmos.redux.mvc.view.events.{GameLost, GamePending, GameWon}
   * @param levelContext object to notify the view of the end game result
   * @param victoryRules enumeration representing the level's victory rules
   */
-case class EndGameSystem(levelContext: GameStateHolder, victoryRules: VictoryRules.Value) extends AbstractSystem2[PlayerCellEntity, DeathProperty] {
+case class EndGameSystem(levelContext: GameStateHolder, victoryRules: VictoryRules.Value) extends AbstractSystem[DeathProperty] {
 
   private val victoryCondition = victoryRules match {
     case VictoryRules.becomeTheBiggest => BecomeTheBiggestVictoryCondition()
@@ -25,12 +25,11 @@ case class EndGameSystem(levelContext: GameStateHolder, victoryRules: VictoryRul
 
   override def update(): Unit = {
     if (levelContext.gameCurrentState == GamePending) {
-      if (entities isEmpty) {
+      val playerEntity = entities.find(_.getTypeComponent.typeEntity == EntityType.Controlled)
+      if (playerEntity isEmpty) {
         levelContext.notify(GameLost)
       } else {
-        entities foreach (playerEntity => {
-          if (victoryCondition.check(playerEntity, entitiesSecondType)) levelContext.notify(GameWon)
-        })
+        if (victoryCondition.check(playerEntity.get, entities)) levelContext.notify(GameWon)
       }
     }
   }
